ecfd2ad6c9f75dafa768d2f289d65df9253e3fc0
Stathis Gennatas <gennatas@gmail.com>
2019-04-14 06:02:08 -0700

    added more tests

--- tests/elevate_tests.R
+++ tests/elevate_tests.R
@@ -9,9 +9,16 @@ x <- rnormmat(100, 5)
 w <- rnorm(5)
 y <- x %*% w + rnorm(100)
 dat <- data.frame(x, y)
-elr <- elevate(dat, n.resamples = 3)
+.test <- requireNamespace("rpart", quietly = TRUE) & requireNamespace("plyr", quietly = TRUE) &
+  requireNamespace("pbapply", quietly = TRUE)
+if (.test) {
+  elr <- elevate(dat, n.resamples = 3, mod = 'cart')
+}
 
 # Classification
 iris2 <- iris[51:150, ]
 iris2$Species <- factor(iris2$Species)
-elc <- elevate(iris2, n.resamples = 3)
+.test <- .test & requireNamespace("pROC", quietly = TRUE)
+if (.test) {
+  elc <- elevate(iris2, n.resamples = 3, mod = 'cart')
+}
