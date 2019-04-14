c5375bf445bb47b41811c5e8421e466554d2de83
Stathis Gennatas <gennatas@gmail.com>
2019-04-14 13:08:50 -0700

    add Select fn

--- tests/crossdecomposition_tests.R
+++ tests/crossdecomposition_tests.R
@@ -6,6 +6,7 @@ library(rtemis)
 x <- rnormmat(400, 500, seed = 2019)
 z <- rnormmat(400, 500, seed = 2020)
 
+xdecomSelect()
 if (requireNamespace("PMA", quietly = TRUE)) {
   xz.cca <- x.CCA(x, z, nperms = 5, permute.niter = 5, n.cores = 1)
 }
