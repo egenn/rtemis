d4ef018c8767261afd14a0b29c00829b32294850
Stathis Gennatas <gennatas@gmail.com>
2019-04-14 13:35:51 -0700

    Added mplot3.xy, mplot3.xym, mplot3.harmonograph, rtRandom

--- tests/staticGraphics_tests.R
+++ tests/staticGraphics_tests.R
@@ -14,23 +14,31 @@ true <- sample(0:1, 20, TRUE)
 estimated <- true
 index <- sample(seq(20), 5)
 estimated[index] <- 1 - as.numeric(estimated[index])
+
 mplot3.adsr()
 mplot3.bar(x)
 mplot3.box(z)
+mplot3.xy(x, y)
+mplot3.xym(x, y)
+
 iris2 <- iris[51:150, ]
 iris2$Species <- factor(iris2$Species)
 mod <- s.CART(iris2, maxdepth = 2)
+
 mplot3.cart(mod)
 mplot3.conf(mod)
 # mplot3.decision(mod, iris2)
 # TODO: INV mplot3.decision
 mplot3.fit(x, x)
 mplot3.fret()
+mplot3.harmonograph()
 mplot3.heatmap(cor(z))
 mplot3.img(z)
 res <- resample(y)
 mplot3.res(res)
 mplot3.roc(mod$fitted.prob, iris2$Species)
 # TODO: fix col for mplot3.roc
+
+rtRandom()
 
 } # if (requireNamespace(...))
