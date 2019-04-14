3ef857175eeb05616c7be806c249d7ad52c27663
Stathis Gennatas <gennatas@gmail.com>
2019-04-14 13:36:05 -0700

    init: Collection of small functions

---
+++ tests/var_tests.R
@@ -0,0 +1,25 @@
+# var_tests.R
+# ::rtemmis::
+#  2019 Efstathios D. Gennatas egenn.github.io
+
+# Library  ====
+library(rtemis)
+
+# Var ====
+
+# '- nCr ====
+nCr(4, 2)
+
+# '- stderror ====
+stderror(rnorm(100))
+
+# '- timeProc ====
+x <- timeProc(sapply(rnorm(100), exp))
+
+# '- typeset ====
+x <- typeset(iris, factor.index = 1, orderedfactor.index = 2, integer.index = 3)
+
+# '- varSelect ====
+x <- rnormmat(100, 10)
+y <- x[, 3]^2 + x[, 5] + x[, 8] * x[, 10]
+vs <- varSelect(x, y)
