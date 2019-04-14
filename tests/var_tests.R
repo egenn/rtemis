e5ee91d04e656a5bffef28759b050a818af9f6a5
Stathis Gennatas <gennatas@gmail.com>
2019-04-14 16:35:43 -0700

    ++

--- tests/var_tests.R
+++ tests/var_tests.R
@@ -7,8 +7,26 @@ library(rtemis)
 
 # Var ====
 dat <- as.data.frame(rnormmmat(50, 2))
+
+# '- cc ====
+cc(iris$Species[1:10], iris$Species[51:60])
+
+# '- classImbalance ====
+classImbalance(iris$Species)
+
+# '- colorOp ====
+colorOp("#8c1515", fn = "invert")
+
+# '- colorPreview ====
+colorPreview(stanfordCol$cardinal)
+
+# '- cols2list ====
+lst <- cols2list(dat)
+
 # '- dat2splinemat ====
-x <- dat2splinemat(dat)
+if (requireNamespace("splines2", quietly = TRUE)) {
+  x <- dat2splinemat(dat)
+}
 
 # '- dat2ploy ====
 x <- dat2poly(dat)
@@ -16,11 +34,31 @@ x <- dat2poly(dat)
 # '- delayTime ====
 delayTime()
 
+# ' drange ====
+drange(runif(10, -10, 10))
+
+# ' fwhm2sigma ====
+fwhm2sigma(8)
+
+# ' gtTable ====
+lst <- list(one = rnorm(10), two = rnorm(10))
+gtTable(lst)
+
+# '- lotri2edgeList
+x <- lotri2edgeList(cor(rnormmat(10, 10)))
+
 # '- nCr ====
 nCr(4, 2)
 
+# '- rsd ====
+rsd(rnorm(20))
+
 # '- stderror ====
 stderror(rnorm(100))
+
+# '- strict ====
+x <- 10
+z <- strict(x, "numeric")
 
 # '- timeProc ====
 x <- timeProc(sapply(rnorm(100), exp))
