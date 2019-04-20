95ad310318c587b3a03a4d19c9967a208b1d3db8
Stathis Gennatas <gennatas@gmail.com>
2019-04-20 02:19:24 -0700

    preparing for future switch [ci skip]

--- R/dataPrepare.R
+++ R/dataPrepare.R
@@ -26,13 +26,16 @@ dataPrepare <- function(x, y, x.test = NULL, y.test = NULL,
                         upsample.seed = NULL,
                         removeDots = FALSE,
                         .preprocess = NULL,
+                        .data.table = FALSE, # for future work
                         verbose = FALSE) {
-  
-  if (!is.list(x)) x <- as.data.frame(x)
+
+  if (!is.list(x)) {
+    x <- if (.data.table) setDT(x) else as.data.frame(x)
+  }
   ncol.x <- NCOL(x)
-  
+
   # x/y, train/test ====
-  
+
   # (x.train, x.test) scenario
   if (!is.null(y) && NCOL(y) > 1 && !inherits(y, "Surv") && NCOL(y) == ncol.x && is.null(x.test) && is.null(y.test)) {
     y.test <- y[, ncol.x]
@@ -40,23 +43,23 @@ dataPrepare <- function(x, y, x.test = NULL, y.test = NULL,
     y <- x[, ncol.x]
     x <- x[, seq(ncol.x - 1)]
   }
-  
+
   # If no y is specified, assume it is the last column of x
   if (is.null(y) & ncol.x > 1) {
     y <- x[, ncol.x]
     x <- x[, seq(ncol.x - 1), drop = FALSE]
   }
-  
+
   if (!is.null(x.test) & is.null(y.test) & NCOL(x.test) == ncol.x) {
     y.test <- x.test[, ncol.x]
     x.test <- x.test[, seq(ncol.x - 1)]
   }
-  
+
   if (!is.null(x.valid) & is.null(y.valid) & NCOL(x.valid) == ncol.x) {
     y.valid <- x.valid[, ncol.x]
     x.valid <- x.valid[, seq(ncol.x - 1)]
   }
-  
+
   # [ preprocess ] ====
   if (!is.null(.preprocess)) {
     .preprocess$x <- x
@@ -66,19 +69,19 @@ dataPrepare <- function(x, y, x.test = NULL, y.test = NULL,
       x.test <- do.call(preprocess, .preprocess)
     }
   }
-  
+
   # If outcome vector is character, convert to factor
   if (class(y) == "character") {
     y <- as.factor(y)
     if (!is.null(y.test)) y.test <- as.factor(y.test)
   }
-  
+
   # If x is vector, convert to matrix - later to data.frame
   if (NCOL(x) == 1 & class(x)[1] != "list") {
     x <- as.matrix(x) # if you use as.data.frame, it will be given colnames
     if (!is.null(x.test)) if (NCOL(x.test) == 1) x.test <- as.matrix(x.test)
   }
-  
+
   # [ xnames and Dimensions check ] ====
   if (class(x)[1] == "list") {
     # for meta models
@@ -86,16 +89,11 @@ dataPrepare <- function(x, y, x.test = NULL, y.test = NULL,
     if (!is.null(x.test)) {
       if (length(x) != length(x.test)) stop("Training and testing feature sets are not of same length")
     }
-    # Extract names from first list element / feature set
-    # if (is.null(colnames(x[[1]]))) {
-    #   xnames <- paste0("Predictor", 1:NCOL(x[[1]]))
-    #   if (!is.null(x.test)) lapply(x.test, function(i) colnames(i) <- paste0("Predictor", 1:NCOL(x[[i]])))
-    # }
     xnames <- c(sapply(x, colnames))
-    
+
     # Remove dots from names if unsupported by algorithm (SparkML)
     if (removeDots) xnames <- gsub("\\.", "_", colnames(x))
-    
+
     x <- lapply(x, as.data.frame)
     if (!is.null(x.test)) {
       x.test <- lapply(x.test, as.data.frame)
@@ -124,7 +122,7 @@ dataPrepare <- function(x, y, x.test = NULL, y.test = NULL,
       stop("Training and testing set do not contain same number of features")
     if (!is.null(y.test)) if (NROW(x.test) != NROW(y.test))
       stop("Testing set features and outcome do not contain same number of cases")
-    
+
     # '- Column names ====
     if (is.null(colnames(x))) {
       xnames <- paste0("Predictor", 1:NCOL(x))
@@ -139,14 +137,14 @@ dataPrepare <- function(x, y, x.test = NULL, y.test = NULL,
       colnames(x.test) <- xnames
     }
   } # //xnames and dimensions check
-  
+
   # [ TYPE ] ====
   if (!is.null(dim(y))) y <- as.vector(y)
   type <- switch(class(y),
                  factor = "Classification",
                  Surv = "Survival",
                  "Regression")
-  
+
   # [ UPSAMPLE: balance outcome class ] ====
   if (type == "Classification" & upsample) {
     if (!is.null(upsample.seed)) set.seed(upsample.seed)
@@ -176,7 +174,7 @@ dataPrepare <- function(x, y, x.test = NULL, y.test = NULL,
   } else {
     x0 <- y0 <- NULL
   }
-  
+
   # [ IPW: Inverse Probability Weighting for Classification ] ====
   class.weights <- weights <- NULL
   if (type == "Classification" & ipw) {
@@ -199,12 +197,12 @@ dataPrepare <- function(x, y, x.test = NULL, y.test = NULL,
     }
   }
   # maybe return weights <- rep(1, NROW(y))
-  
+
   # [ SURVIVAL ] ====
   if (survival::is.Surv(y)) {
     type <- "Survival"
   }
-  
+
   # [ OUTRO ] ====
   list(x = x, y = y,
        x.test = x.test, y.test = y.test,
@@ -212,5 +210,5 @@ dataPrepare <- function(x, y, x.test = NULL, y.test = NULL,
        x0 = x0, y0 = y0,
        xnames = xnames, type = type,
        class.weights = class.weights, weights = weights)
-  
+
 } # rtemis::dataPrepare
