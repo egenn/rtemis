bac53499a9b1e090dbacbda177ee34033ee7ee7b
Stathis Gennatas <gennatas@gmail.com>
2019-05-05 10:31:22 -0700

    added all current palettes to rtPalette [ci skip]

--- R/rtPalette.R
+++ R/rtPalette.R
@@ -201,7 +201,7 @@ usfCol <- list(green = "#205C40",
 
 # UC San Diego ====
 #' UC San Diego Colors
-#' 
+#'
 #' \code{ucsdCol}: UC San Diego color palette (https://ucpa.ucsd.edu/brand/elements/color-palette/)
 #' @name rtPalettes
 #' @export
@@ -221,7 +221,7 @@ ucsdCol <- list(blue = "#182B49",
 
 # UCLA ====
 #' UCLA Colors
-#' 
+#'
 #' \code{uclaCol}: UCLA color palette (http://brand.ucla.edu/identity/colors)
 #' @name rtPalettes
 #' @export
@@ -243,8 +243,8 @@ uclaCol <- list(blue = "#2774AE",
 
 # University of California ====
 #' University of California Colors
-#' 
-#' \code{ucCol}: University of California color palette 
+#'
+#' \code{ucCol}: University of California color palette
 #' (http://brand.universityofcalifornia.edu/guidelines/color.html#!primary-colors)
 #' @name rtPalettes
 #' @export
@@ -270,8 +270,8 @@ ucCol <- list(ucBlue = "#1295D8",
 
 # Washington ====
 #' University of Washington Colors
-#' 
-#' \code{uwCol}: University of Washington color palette 
+#'
+#' \code{uwCol}: University of Washington color palette
 #' (http://www.washington.edu/brand/graphic-elements/primary-color-palette/)
 #' @name rtPalettes
 #' @export
@@ -283,7 +283,7 @@ uwCol <- list(purple = "#4b2e83",
 
 # NIH ====
 #' NIH Colors
-#' 
+#'
 #' \code{nihCol}: NIH color palette (https://www.nlm.nih.gov/about/nlm_logo_guidelines_030414_508.pdf)
 #' @name rtPalettes
 #' @export
@@ -294,8 +294,8 @@ nihCol <- list(blue = "#20558a",
 
 # Apple ====
 #' Apple Colors
-#' 
-#' \code{appleCol}: Apple Human Interface Guidelines color palette 
+#'
+#' \code{appleCol}: Apple Human Interface Guidelines color palette
 #' (https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/color/)
 #' @name rtPalettes
 #' @export
@@ -312,7 +312,7 @@ appleCol <- list(red = "#FF3B30",
 
 # Google ====
 #' Google Colors
-#' 
+#'
 #' \code{googleCol}: Google brand palette (https://brandpalettes.com/google-colors/)
 #' @name rtPalettes
 #' @export
@@ -324,8 +324,8 @@ googleCol <- list(blue = "#4285F4",
 
 # Amazon ====
 #' Amazon Colors
-#' 
-#' \code{amazonCol}: Amazon brand palette 
+#'
+#' \code{amazonCol}: Amazon brand palette
 #' (https://images-na.ssl-images-amazon.com/images/G/01/AdvertisingSite/pdfs/AmazonBrandUsageGuidelines.pdf)
 #' @name rtPalettes
 #' @export
@@ -336,8 +336,8 @@ amazonCol <- list(orange = "#FF9900",
 
 # Microsoft ====
 #' Microsoft Colors
-#' 
-#' \code{microsoftCol}: Microsoft brand palette 
+#'
+#' \code{microsoftCol}: Microsoft brand palette
 #' (https://brandcolors.net/b/microsoft)
 #' @name rtPalettes
 #' @export
@@ -365,31 +365,33 @@ rtPalettes <- list(pennCol = pennCol,
                    microsoftCol = microsoftCol)
 
 #' \pkg{rtemis} Color Palettes
-#' 
+#'
 #' \code{rtPalettes} prints names of available color palettes
 #' Each palette is a named list of hexadecimal color definitions which can be used with any
 #' graphics function.
 #' @param palette String: Name of palette to return. Default = NULL: available palette names
 #' are printed and no palette is returned
-#' @return 
+#' @return
 #' A list of available palettes, invisibly
-#' @examples 
+#' @examples
 #' rtPalette()
 #' @export
 
 rtPalette <- function(palette = NULL) {
-  
+
   if (is.null(palette)) {
     msg(crayon::cyan("The following palettes are available:"))
-    print(paste(c("pennCol", "imperialCol", "ucsfCol", "berkeleyCol", 
-                  "stanfordCol", "ucsdCol", "uclaCol", "usfCol", "nihCol")))
+    print(paste(c("imperialCol", "pennCol", "stanfordCol", "ucCol", "ucsfCol", "berkeleyCol",
+                   "ucsdCol", "uclaCol", "usfCol", "uwCol", "nihCol", "appleCol", "googleCol",
+                  "amazonCol", "microsoftCol")))
   } else {
     palette <- match.arg(palette,
-                         c("pennCol", "imperialCol", "ucsfCol", "ucsfPalette",
-                           "berkeleyCol", "stanfordCol", "usfCol", "ucsdCol", "uclaCol", "nihCol"))
+                         c("imperialCol", "pennCol", "stanfordCol", "ucCol", "ucsfCol", "berkeleyCol",
+                           "ucsdCol", "uclaCol", "usfCol", "uwCol", "nihCol", "appleCol", "googleCol",
+                           "amazonCol", "microsoftCol"))
     rtPalettes[[palette]]
   }
-  
+
 } # rtemis::rtPalettes
 
 
