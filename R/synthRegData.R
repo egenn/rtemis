8b4ed5fd30cf3442a815bf2e79c6d765bf32cf75
Stathis Gennatas <gennatas@gmail.com>
2019-05-07 03:03:41 -0700

    export synthRegData

--- R/synthRegData.R
+++ R/synthRegData.R
@@ -2,6 +2,14 @@
 # ::rtemis::
 # 2019 Efstathios D. Gennatas egenn.github.io
 
+#' Synthesize Simple Regression Data
+#'
+#' @param nrow Integer: Number of rows. Default = 500
+#' @param ncol Integer: Number of columns. Default = 50
+#' @param seed Integer: Seed for random number generator. Default = NULL
+#' @author Efstathios D. Gennatas
+#' @export
+
 synthRegData <- function(nrow = 500, ncol = 50, seed = NULL) {
 
   if (!is.null(seed)) set.seed(seed)
