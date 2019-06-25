0325a841bee9a43cd0dd20f2430cc7bd3131b4af
Stathis Gennatas <gennatas@gmail.com>
2019-06-25 13:47:26 -0400

    Added verbose arg to rt_lle_calc [ci skip]

--- R/rt_lle_calc_k.R
+++ R/rt_lle_calc_k.R
@@ -3,24 +3,25 @@
 # 2019 Efstathios D. Gennatas egenn.github.io
 
 #' \pkg{rtemis} internal: \code{lle::calc_k} function adapted to work with \code{pbapply}
-#' 
+#'
 #' @keywords internal
 
 rt_lle_calc_k <- function(X, m,
                           kmin = 1, kmax = 20,
                           plotres = TRUE,
-                          n.cores = rtCores, 
-                          iLLE = FALSE) {
+                          iLLE = FALSE,
+                          n.cores = rtCores,
+                          verbose = TRUE) {
   N <- dim(X)[1]
-  if (kmax >= N) 
+  if (kmax >= N)
     kmax <- N - 1
   # if (.Platform$OS.type == "windows") {
   #   dev <- "nul"
   # } else {
   #   dev <- "/dev/null"
   # }
-  
-  # if (parallel == TRUE) 
+
+  # if (parallel == TRUE)
   #   sfInit(parallel = TRUE, cpus = cpus)
   # else sfInit(parallel = FALSE)
   # options(warn = -1)
@@ -37,9 +38,9 @@ rt_lle_calc_k <- function(X, m,
     for (i in 1:N) rho <- c(rho, cor(Dx[i, ], Dy[i, ]))
     return(mean(1 - rho^2))
   }
-  # rho <- invisible(sfLapply(kmin:kmax, perform_calc, X, m, 
+  # rho <- invisible(sfLapply(kmin:kmax, perform_calc, X, m,
   #                           iLLE))
-  
+
   # rho <- pbapply::pblapply(kmin:kmax, function(i) perform_calc(k = i,
   #                                                              X = X,
   #                                                              m = m,
@@ -50,17 +51,17 @@ rt_lle_calc_k <- function(X, m,
                                                                m = m,
                                                                iLLE = iLLE),
                            cl = n.cores)
-  
+
   # rho <- unclass(unlist(rho))
   res <- data.frame(k = c(kmin:kmax), rho = rho)
   if (plotres) {
     par(mar = c(5, 5, 4, 2) + 0.1)
-    plot(res$k, res$rho, type = "b", xlab = "k", ylab = expression(1 - 
+    plot(res$k, res$rho, type = "b", xlab = "k", ylab = expression(1 -
                                                                      rho^2), main = "")
     abline(h = min(res$rho, na.rm = TRUE), col = "red")
     grid()
   }
-  else cat("best k:", head(res$k[order(res$rho)], 3), "\n\n")
+  if (verbose) cat("best k:", head(res$k[order(res$rho)], 3), "\n\n")
   res
-  
+
 } # rtemis::rt_lle_calc_k
