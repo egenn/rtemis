2ab7f65366f382da7cf3a6f8297767ef52971f65
Stathis Gennatas <gennatas@gmail.com>
2019-07-22 19:58:41 -0400

    Added resampling [ci skip]

--- R/synthRegData.R
+++ R/synthRegData.R
@@ -6,11 +6,17 @@
 #'
 #' @param nrow Integer: Number of rows. Default = 500
 #' @param ncol Integer: Number of columns. Default = 50
+#' @param resample.rtset Output of \link{rtset.resample} defining training/testing split. The first resulting resample
+#' will be used to create \code{dat.train} and \code{dat.test} output; all resample output under \code{resamples}
 #' @param seed Integer: Seed for random number generator. Default = NULL
 #' @author Efstathios D. Gennatas
+#' @return List with elements \code{dat, dat.train, dat.test, resamples, w, seed}
 #' @export
 
-synthRegData <- function(nrow = 500, ncol = 50, seed = NULL) {
+synthRegData <- function(nrow = 500,
+                         ncol = 50,
+                         resample.rtset = rtset.resample(),
+                         seed = NULL) {
 
   if (!is.null(seed)) set.seed(seed)
   x <- rnormmat(nrow, ncol)
@@ -19,7 +25,14 @@ synthRegData <- function(nrow = 500, ncol = 50, seed = NULL) {
   dat <- data.frame(x, y)
   colnames(dat)[seq(ncol)] <- paste0("Feature_", seq(ncol))
 
+  res <- resample(y, rtset = resample.rtset)
+  dat.train <- dat[res[[1]], ]
+  dat.test <- dat[-res[[1]], ]
+
   list(dat = dat,
+       dat.train = dat.train,
+       dat.test = dat.test,
+       resamples = res,
        w = w,
        seed = seed)
 
