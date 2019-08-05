0db99ee7561e39a2e60d12fd4cb23c243522d000
Stathis Gennatas <gennatas@gmail.com>
2019-08-05 12:09:27 -0400

    Split to vector [default] and data.frame methods

--- R/oneHot.R
+++ R/oneHot.R
@@ -4,28 +4,17 @@
 
 #' One hot encoding
 #'
-#' One hot encode all factors in a data.frame
+#' One hot encode a vector or all factors in a data.frame
 #'
-#' @param x data.frame
+#' @param x Vector or data.frame
 #' @param verbose Logical: If TRUE, print messages to console. Default = TRUE
-#' @return data.frame where all factors are one hot encoded
+#' @return For vector input, a one-hot-encoded matrix, for data.frame frame input, a data.frame where all factors are
+#' one-hot encoded
 #' @author Efstathios D Gennatas
 #' @export
 
 oneHot <- function(x, verbose = TRUE) {
-  ncases <- NROW(x)
-  factor.index <- which(sapply(x, is.factor))
-  one.hot <- as.list(x)
-  if (verbose) .names <- colnames(x)
-  for (i in factor.index) {
-    if (verbose) msg0("One hot encoding ", .names[i], "...")
-    .levels <- levels(x[, i])
-    index <- as.numeric(x[, i])
-    oh <- matrix(0, ncases, length(.levels))
-    colnames(oh) <- .levels
-    for (j in seq(ncases)) oh[j, index[j]] <- 1
-    one.hot[[i]] <- oh
-  }
-  if (verbose) msg("Done")
-  as.data.frame(one.hot)
+
+  UseMethod("oneHot", x)
+
 } # rtemis::oneHot
