cf3b9de0f175705f2a78520fae8093e1ebccc394
Stathis Gennatas <gennatas@gmail.com>
2019-08-23 07:25:18 -0400

    Added interactive graphics tests

---
+++ tests/interactiveGraphics_tests.R
@@ -0,0 +1,14 @@
+# interactiveGraphics_tests.R
+# ::rtemis::
+# 2019 Efstathios D. Gennatas egenn.github.io
+
+if (requireNamespace("plotly", quietly = TRUE)) {
+  dplot3.bar(VADeaths)
+  dplot3.box(VADeaths)
+  dplot3.x(split(iris$Sepal.Length, iris$Species))
+  dplot3.xy(iris$Sepal.Length, iris$Petal.Length, fit = "gam", se.fit = TRUE, group = iris$Species)
+  dplot3.heatmap(cor(rnormmat(10, 10, seed = 2019)))
+  varimp <- rnorm(10)
+  names(varimp) <- paste0("Feature_", seq(10))
+  dplot3.varimp(varimp)
+}
