3ea4f925d45e59288f5913963dc4d24b311abd4c
Stathis Gennatas <gennatas@gmail.com>
2019-09-28 16:22:38 -0400

    docs [ci skip]

--- R/oneHot.R
+++ R/oneHot.R
@@ -7,7 +7,7 @@
 #' One hot encode a vector or factors in a data.frame
 #'
 #' A vector input will be one-hot encoded regardless of type by looking at all unique values. With data.frame input,
-#' only column of type factor will be one-hot encoded. This function is used by \link{preprocess(oneHot = TRUE)}
+#' only column of type factor will be one-hot encoded. This function is used by \link{preprocess}
 #' @param x Vector or data.frame
 #' @param verbose Logical: If TRUE, print messages to console. Default = TRUE
 #' @return For vector input, a one-hot-encoded matrix, for data.frame frame input, an expanded data.frame where all
