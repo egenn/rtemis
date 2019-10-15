d543a00034f8fbb107852364b7d0f8b29b1bf641
Stathis Gennatas <gennatas@gmail.com>
2019-10-15 02:44:17 -0700

    cleanup [ci skip]

--- R/mplot3.addtree.R
+++ R/mplot3.addtree.R
@@ -3,40 +3,40 @@
 # Efstathios D. Gennatas egenn.github.io
 
 #' \code{mplot3} ADDTREE trees
-#' 
+#'
 #' Plot ADDTREE trees using \code{data.tree::plot.Node}
-#' 
+#'
 #' @author Efstathios D. Gennatas
 #' @export
 
 mplot3.addtree <- function(addtree,
-                       col.positive = "#F48024DD",
-                       col.negative = "#18A3ACDD",
-                       node.col = "#666666",
-                       node.shape = "none",
-                       node.labels = TRUE,
-                       node.labels.pct.pos = NULL,
-                       pos.name = NULL,
-                       edge.col = "#999999",
-                       edge.width = 2,
-                       edge.labels = FALSE,
-                       arrowhead = "vee",
-                       layout = "dot",
-                       drop.leaves = FALSE,
-                       rankdir = "TB",
-                       splines = "polyline",
-                       fontname = "helvetica",
-                       bg.color = "white",
-                       overlap = "false",
-                       prune = NULL,
-                       prune.empty.leaves = TRUE,
-                       remove.bad.parents = FALSE) {
+                           col.positive = "#F48024DD",
+                           col.negative = "#18A3ACDD",
+                           node.col = "#666666",
+                           node.shape = "none",
+                           node.labels = TRUE,
+                           node.labels.pct.pos = NULL,
+                           pos.name = NULL,
+                           edge.col = "#999999",
+                           edge.width = 2,
+                           edge.labels = FALSE,
+                           arrowhead = "vee",
+                           layout = "dot",
+                           drop.leaves = FALSE,
+                           rankdir = "TB",
+                           splines = "polyline",
+                           fontname = "helvetica",
+                           bg.color = "white",
+                           overlap = "false",
+                           prune = NULL,
+                           prune.empty.leaves = TRUE,
+                           remove.bad.parents = FALSE) {
 
   # [ DEPENDENCIES ] ====
   if (!depCheck("data.tree", verbose = FALSE)) {
     cat("\n"); stop("Please install dependencies and try again")
   }
-  
+
   # [ TREE ] ====
   if (inherits(addtree, "Node")) {
     addtree <- data.tree::Clone(addtree)
@@ -51,19 +51,19 @@ mplot3.addtree <- function(addtree,
       if (is.null(prune)) prune <- FALSE
     }
   }
-  
+
   # [ ARGUMENTS ] ====
   if (is.null(node.labels.pct.pos)) {
     node.labels.pct.pos <- if (is.null(addtree$pct.pos)) FALSE else TRUE
   }
-  
+
   # [ PRUNE ] ====
   if (prune) {
     addtree <- prune.addtree(addtree,
-                          prune.empty.leaves = prune.empty.leaves,
-                          remove.bad.parents = remove.bad.parents)
+                             prune.empty.leaves = prune.empty.leaves,
+                             remove.bad.parents = remove.bad.parents)
   }
-  
+
   # [ GPARH STYLE ] ====
   data.tree::SetGraphStyle(addtree,
                            layout = layout,
@@ -78,8 +78,8 @@ mplot3.addtree <- function(addtree,
   # [ NODE STYLE ] ====
   node.labels <- if (node.labels) {
     if (node.labels.pct.pos) {
-      function(node) paste(gsub("[{}]", "", node$Condition), 
-                           paste("N =", node$N), 
+      function(node) paste(gsub("[{}]", "", node$Condition),
+                           paste("N =", node$N),
                            paste(paste0(ddSci(node$pct.pos, asNumeric = TRUE) * 100, "%"), pos.name),
                            sep = "\n")
     } else {
@@ -93,13 +93,13 @@ mplot3.addtree <- function(addtree,
                           shape = node.shape,
                           fillcolor = node.col,
                           col = node.col,
-                          fontname = fontname, 
+                          fontname = fontname,
                           label = node.labels,
-                          tooltip = function(node) paste(paste("Node", node$n), 
-                                                         paste("Value =", ddSci(node$Value)), 
+                          tooltip = function(node) paste(paste("Node", node$n),
+                                                         paste("Value =", ddSci(node$Value)),
                                                          sep = "\n"),
                           rank = function(node) node$Depth)
-  
+
   # [ EDGE STYLE ] ====
   edge.labels <- if (edge.labels) function(node) node$Condition else NULL
   data.tree::SetEdgeStyle(addtree,
