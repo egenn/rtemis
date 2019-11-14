4a43fff6275b601c4d36242b75e32449a8a4d359
Stathis Gennatas <gennatas@gmail.com>
2019-11-13 23:50:31 -0800

    cleanup [ci skip]

--- R/checkData.R
+++ R/checkData.R
@@ -1,10 +1,10 @@
 # checkData.R
 # ::rtemis::
-# 2017 Efstathios D. Gennatas egenn.github.io
+# 2017-9 Efstathios D. Gennatas egenn.github.io
 
 #' Check Data
 #'
-#' Performs a series of simple checks on a dataset that may be important to perform ahead of
+#' Runs a series of simple checks on a dataset that may be important to perform ahead of
 #' data analysis. This function makes no changes to data, but reports potential course of action
 #' that can be taken using \link{preprocess}
 #'
@@ -38,8 +38,8 @@ checkData <- function(x,
   # Dimensions
   n.rows <- NROW(x)
   n.cols <- NCOL(x)
-  cat(" ", bold(n.rows), ifelse(n.rows == 1, "case", "cases"),
-      "with", bold(n.cols), ifelse(n.cols == 1, "feature:", "features:"), "\n")
+  cat(" ", bold(n.rows), ngettext(n.rows, "case", "cases"),
+      "with", bold(n.cols), ngettext(n.cols, "feature:", "features:"), "\n")
 
   # [ Integers ] ====
   index.integer <- which(sapply(x, is.integer))
@@ -54,23 +54,23 @@ checkData <- function(x,
   n.factor <- length(index.factor)
   index.ordered <- which(sapply(x, is.ordered))
   n.ordered <- length(index.ordered)
-  cat("  *", bold(n.continuous), "continuous", ifelse(n.continuous == 1, "feature", "features"), "\n")
-  cat("  *", bold(n.integer), "integer", ifelse(n.integer == 1, "feature", "features"), "\n")
+  cat("  *", bold(n.continuous), "continuous", ngettext(n.continuous, "feature", "features"), "\n")
+  cat("  *", bold(n.integer), "integer", ngettext(n.integer, "feature", "features"), "\n")
   isOrdered <- if (n.factor == 1) {
-    paste(", which", ifelse(n.ordered == 1, "is", "is not"), "ordered")
+    paste(", which", ngettext(n.ordered, "is", "is not"), "ordered")
   } else if (n.factor > 1) {
-    paste(", of which", bold(n.ordered), ifelse(n.ordered == 1, "is", "are"), "ordered")
+    paste(", of which", bold(n.ordered), ngettext(n.ordered, "is", "are"), "ordered")
   } else {
     ""
   }
-  cat("  * ", bold(n.factor), " categorical", ifelse(n.factor == 1, " feature", " features"),
+  cat("  * ", bold(n.factor), " categorical", ngettext(n.factor, " feature", " features"),
       isOrdered, "\n", sep = "")
   # n.levels.nonordered <- sapply(x[, setdiff(index.factor, index.ordered)], function(x) length(levels(x)))
   index.gt2levels.nonordered <- which(sapply(x[, setdiff(index.factor, index.ordered), drop = FALSE], function(x) length(levels(x))) > 2)
   n.gt2levels.nonordered <- length(index.gt2levels.nonordered)
   if (n.gt2levels.nonordered > 0) {
     cat("    **", bold(n.gt2levels.nonordered), "unordered categorical",
-        ifelse(n.gt2levels.nonordered == 1, "feature has", "features have"),
+        ngettext(n.gt2levels.nonordered, "feature has", "features have"),
         "more than 2 levels\n")
   }
 
@@ -78,13 +78,13 @@ checkData <- function(x,
   index.constant <- which(sapply(x, is.constant))
   n.constant <- length(index.constant)
   .col <- if (n.constant > 0) red$bold else bold
-  cat("  *", .col(n.constant), "constant", ifelse(n.constant == 1, "feature", "features"), "\n")
+  cat("  *", .col(n.constant), "constant", ngettext(n.constant, "feature", "features"), "\n")
 
   # [ Duplicates ] ====
   cindex.dups <- which(duplicated(x))
   n.dups <- length(cindex.dups)
   .col <- if (n.dups > 0) red$bold else bold
-  cat("  *", .col(n.dups), "duplicated", ifelse(n.dups == 1, "case", "cases"), "\n")
+  cat("  *", .col(n.dups), "duplicated", ngettext(n.dups, "case", "cases"), "\n")
 
   # [ NAs ] ====
   cols.anyna <- which(sapply(x, anyNA))
@@ -106,8 +106,8 @@ checkData <- function(x,
 
   }
   .col <- if (n.cols.anyna > 0) rtOrange$bold else bold
-  cat("  * ", .col(n.cols.anyna), ifelse(n.cols.anyna == 1, " feature includes", " features include"), " 'NA' values",
-      ifelse(n.cols.anyna > 0, paste(";", .col(n.na), "'NA'", ifelse(n.na == 1, "value", "values"), "total"), ""),
+  cat("  * ", .col(n.cols.anyna), ngettext(n.cols.anyna, " feature includes", " features include"), " 'NA' values",
+      ifelse(n.cols.anyna > 0, paste(";", .col(n.na), "'NA'", ngettext(n.na, "value", "values"), "total"), ""),
       "\n", sep = "")
   if (n.cols.anyna > 0) {
     if (!is.null(reportFeatures.thres)) {
@@ -129,7 +129,7 @@ checkData <- function(x,
       cases.na.over.thres <- na.case.pct[na.case.pct$Pct.NA >= reportCases.thres, ]
       n.cases.na.over.thres <- NROW(cases.na.over.thres)
       if (n.cases.na.over.thres > 0 ) {
-        cat("    ** ", .col(n.cases.na.over.thres), ifelse(n.cases.na.over.thres > 1, " cases", " case"),
+        cat("    ** ", .col(n.cases.na.over.thres), ngettext(n.cases.na.over.thres, " case", " cases"),
             " missing >= ", reportCases.thres*100, "% of features:\n      *** ",
             paste0("#", cases.na.over.thres$Case, ": ", ddSci(cases.na.over.thres$Pct.NA),
                    collapse = "\n      *** "), "\n",
@@ -154,11 +154,11 @@ checkData <- function(x,
     boxcat("Recommendations", pad = 2)
     if (n.constant > 0 | n.cols.anyna > 0 | n.gt2levels.nonordered > 0) {
       if (n.constant > 0) {
-        cat(rtHighlight$bold("  * Remove the constant", ifelse(n.constant == 1, "feature", "features"), "\n"))
+        cat(rtHighlight$bold("  * Remove the constant", ngettext(n.constant, "feature", "features"), "\n"))
       }
 
       if (n.dups > 0) {
-        cat(rtHighlight$bold("  * Remove the duplicated", ifelse(n.dups == 1, "case", "cases"), "\n"))
+        cat(rtHighlight$bold("  * Remove the duplicated", ngettext(n.dups, "case", "cases"), "\n"))
       }
 
       if (n.cols.anyna > 0) {
@@ -173,9 +173,9 @@ checkData <- function(x,
       }
       if (n.integer > 0) {
         cat(rtHighlight$bold("  * Check the", ifelse(n.integer > 1, paste("", n.integer, ""), " "),
-            "integer", ifelse(n.integer > 1, " features", " feature"),
-            " and consider if", ifelse(n.integer > 1, " they", " it"), " should be converted to ",
-            ifelse(n.integer > 1, "factors\n", "factor\n"), sep = ""))
+            "integer", ngettext(n.integer, " feature", " features"),
+            " and consider if", ngettext(n.integer, " it", " they"), " should be converted to ",
+            ngettext(n.integer, "factor\n", "factors\n"), sep = ""))
       }
     } else {
       cat(green$bold("  * Everything looks good\n"))
