62f896b17f8311d2b4b69e11c9e2df357528a0b5
Stathis Gennatas <gennatas@gmail.com>
2020-02-27 22:06:01 -0800

    init [ci skip]

---
+++ R/savePMML.R
@@ -0,0 +1,24 @@
+# savePMML.R
+# ::rtemis::
+# 2020 Efstathios D. Gennatas egenn.github.io
+
+#' Save rtemis model to PMML file
+#'
+#' @param x rtemis model
+#' @param filename Character: path to file
+#' @export
+#' @author Efstathios D. Gennatas
+savePMML <- function(x, filename) {
+
+  if (!depCheck("pmml", verbose = FALSE)) {
+    cat("\n"); stop("Please install dependencies and try again")
+  }
+
+  supported <- c("GLM", "GBM", "CART", "SVM", "RF", "RFSRC")
+  if (!x$mod.name %in% supported) stop("Unsupported model")
+
+  mod_pmml <- pmml::pmml(x$mod)
+
+  pmml::save_pmml(mod_pmml, name = filename)
+
+} # rtemis::savePMML
