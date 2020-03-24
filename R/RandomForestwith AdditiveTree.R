fefce3ecbec024f1b34c001083b65acc83e7c465
NourOmran393 <62600316+NourOmran393@users.noreply.github.com>
2020-03-24 14:01:44 +0200

    Create RandomForestwith AdditiveTree.R

---
+++ R/RandomForestwith AdditiveTree.R
@@ -0,0 +1,46 @@
+# Random Forest Model -----------------------------------------------------
+
+trainRandomForest <- function(data,
+                              response,
+                              predictors = names(data)[names(data) != response],    # Default to all variables except the response variable
+                              requiredCostReduction = 0.2,                          # Defualt to 0.2
+                              samplePredictorCount = floor(length(predictors)^0.5), # Defualt to m = sqrt(p)
+                              treeCount,                                            # NEW to determine number of decision trees to grow
+                              bootstrapRatio = 0.8){                                # NEW to determine bootstrap sample size
+  
+  # A bit of housekeeping ---------------------------------------------------
+  
+  # Set progress bar
+  pb <- txtProgressBar(min = 0, max = treeCount, style = 3)
+  
+  # Initialize list of n trees
+  output <- vector("list", length = treeCount)
+  
+  # Name each element for readibility
+  names(output) <- c(1:treeCount)
+  
+  
+  # Grow random forest ------------------------------------------------------
+  
+  # Loop through our treeCount variable to grow n trees
+  for(i in 1:treeCount){
+    
+    # Update progress bar
+    setTxtProgressBar(pb, i)
+    
+    # Perform our bootstrap selection of observations
+    sample.data <- data[sample(nrow(data),floor(nrow(data)*bootstrapRatio), replace = TRUE),]
+    
+    # Train decision tree i on our bootstrapped data
+    output[[i]] <- s.ADDTREE(sample.data, gamma = .8, learning.rate = .1)
+  
+  }
+  
+  # Close progress bar
+  close(pb)
+  
+  # Return list of decision trees
+  output
+  
+}
+# Finished!
