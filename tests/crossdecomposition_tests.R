01a5a73e1211ecef48278539b77d4f98907d6fa2
Stathis Gennatas <gennatas@gmail.com>
2020-06-24 22:39:11 -0700

    temp remove ANTsR

--- tests/crossdecomposition_tests.R
+++ tests/crossdecomposition_tests.R
@@ -2,7 +2,6 @@
 # ::rtemis::
 # 2019 Efstathios D. Gennatas egenn.github.io
 
-suppressWarnings(RNGversion("3.5.0"))
 library(rtemis)
 x <- rnormmat(400, 500, seed = 2019)
 z <- rnormmat(400, 500, seed = 2020)
@@ -12,6 +11,6 @@ if (requireNamespace("PMA", quietly = TRUE)) {
   xz.cca <- x.CCA(x, z, nperms = 5, permute.niter = 5, n.cores = 1)
 }
 
-if (requireNamespace("ANTsR", quietly = TRUE)) {
-  xz.sd2res <- x.SD2RES(x, z, n.res = 10)
-}
+# if (requireNamespace("ANTsR", quietly = TRUE)) {
+#   xz.sd2res <- x.SD2RES(x, z, n.res = 10)
+# }
