e4d4ac1921e1fac763f6643bbf42cc9b6d4f5587
Stathis Gennatas <gennatas@gmail.com>
2020-07-22 21:55:02 -0700

    init rstudio theme

---
+++ R/rstudio_theme_rtemis.R
@@ -0,0 +1,36 @@
+# rtemis RStudio theme
+# ::rtemis::
+# Efstathios D. Gennatas egenn.github.io
+
+#' Set lambda theme for RStudio
+#'
+#' @export
+
+rstudio_theme_rtemis <- function() {
+
+  # Check we are in RStudio
+  if (!rstudioapi::isAvailable()) {
+    stop("Please run this function from within RStudio")
+  }
+
+  if (rstudioapi::getThemeInfo()$editor == "rtemis") {
+    msg("rtemis theme already in use")
+  } else {
+    if ("rtemis" %in% rstudioapi::getThemes()) {
+      msg("rtemis theme already installed")
+    } else {
+      # '- Add theme ====
+      # check RStudio supports themes
+      if (compareVersion(as.character(rstudioapi::versionInfo()$version), "1.2.0") == -1) {
+        stop("Please update to the latest RStudio version (or at least 1.2.0)")
+      }
+      msg("Adding rtemis theme to RStudio...")
+      rstudioapi::addTheme(
+        system.file(fs::path("resources","rtemis.rstheme"), package = "rtemis")
+      )
+    }
+    # '- Activate theme ====
+    rstudioapi::applyTheme("rtemis")
+  }
+
+} # rtemis::rstudio_theme_rtemis
