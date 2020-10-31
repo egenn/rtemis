a65bce061be9e847cee9dd98e99c39ebcdf25051
Stathis Gennatas <gennatas@gmail.com>
2020-10-31 16:42:56 -0700

    init [ci skip]

---
+++ R/factor_NA2missing.R
@@ -0,0 +1,31 @@
+# factor_NA2missing.R
+# ::rtemis::
+# 2020 Efstathios D. Gennatas rtemis.lambdamd.org
+
+#' Factor NA to "missing" level
+#'
+#' Set NA values of a factor vector to a new level indicating missingness
+#'
+#' @param x Factor
+#' @param na_level_name Character: Name of new level to create that will be assigned to all current
+#' NA values. Default = "missing"
+#' @author Efstathios D. Gennatas
+#' @export
+#' @examples
+#' x <- factor(sample(letters[1:3], 100, T))
+#' x[sample(1:100, 10)] <- NA
+#' xm <- factor_NAtolevel(x)
+
+factor_NA2missing <- function(x, na_level_name = "missing") {
+
+  if (!is.factor(x)) stop("x must be a factor, instead it is of class", class(x))
+  if (anyNA(x)) {
+    x <- factor(x, levels = c(levels(x), na_level_name))
+    x[is.na(x)] <- "missing"
+    x
+  } else {
+    x
+  }
+
+
+} # rtemis::factor_NA2missing
