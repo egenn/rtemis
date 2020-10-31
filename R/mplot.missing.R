564b9f41ccc33c5fe0cff24b118be1a4549a7061
Stathis Gennatas <gennatas@gmail.com>
2020-10-31 16:44:15 -0700

    fix autopositioning of labels [ci skip]

--- R/mplot.missing.R
+++ R/mplot.missing.R
@@ -26,60 +26,75 @@ mplot.missing <- function(x,
                           col.present = "#18A3AC",
                           col.missing = "#F48024",
                           feat.names.y = 0,
-                          x.srt = 65,
+                          names.srt = 90,
                           x.tick.labs.adj = 0,
-                          case.names.x = 0,
-                          case.names.every = 20,
+                          case.names.x = .25,
+                          case.names.every = NULL,
                           y.srt = 0,
                           mar = c(3, 3.5, 5.5, 1),
                           oma = c(.5, .5, .5, .5),
                           par.reset = TRUE, ...) {
 
-
+  # Arguments ====
   if (is.null(feat.names)) feat.names <- colnames(x)
   if (is.null(case.names)) case.names <- rownames(x)
   if (is.null(main)) main <- paste("Missing Data in", deparse(substitute(x)))
+  if (is.null(case.names.every)) case.names.every <- floor(NROW(x) * .1)
 
   par.orig <- par(no.readonly = TRUE)
   if (par.reset) on.exit(par(par.orig))
-
   par(mar = mar, oma = oma)
 
-  # NAperColumn <- sapply(x, function(i) sum(is.na(i)))
   xnai <- matrix(as.integer(is.na(x)), NROW(x))
   NAperColumn <- apply(xnai, 2, sum)
-  x <- 1:NCOL(xnai)
-  y <- 1:NROW(xnai)
+  x <- seq_len(NCOL(xnai))
+  y <- seq_len(NROW(xnai))
   z <- t(apply(xnai, 2, rev))
+
+  ylim <- c(.5, max(y) + .5)
+  xlim <- c(.5, max(x) + .5)
 
   image(x, y, data.matrix(z),
         col = c(col.present, col.missing),
         zlim = c(0, 1),
-        asp = NULL, ann = FALSE, axes = FALSE)
-  mtext(main, side = 1, font = 2, line = 1.7)
+        asp = NULL, ann = FALSE, axes = FALSE,
+        mar = c(0, 0, 0, 0),
+        oma = c(0, 0, 0, 0))
+
+  # title below image
+  mtext(main, side = 1, font = 2, line = 2)
+
+  # column names above image
   text(x,
-       max(y) + diff(range(y)) * .02,
+       ylim[2] + diff(range(y)) * .04,
        labels = feat.names,
        adj = 0,
-       # pos = 4,
        offset = 0,
-       # cex = cex.x,
-       # col = xnames.col,
        xpd = TRUE,
-       srt = x.srt)
+       srt = names.srt)
+
+  # row index left of image
   indl <- logical(case.names.every)
   indl[case.names.every] <- TRUE
-  text(x = case.names.x, y[rev(indl)],
+  text(x = xlim[1] - diff(range(x)) * .02,
+       y[rev(indl)],
        labels = rev(case.names)[rev(indl)],
-       adj = 0,
+       adj = 1,
        xpd = TRUE)
-  mtext("Case", 2, line = 2.6)
-  lo.y <- 0 - diff(range(y)) * .06
-  polygon(c(0.5, 0.5, max(x) + .5, max(x) + .5), c(lo.y, 0, 0, lo.y),
+
+  # ylab
+  mtext("Case (row nr.)", 2, line = 2.5)
+
+  # Column counts below image
+  lo.y <- ylim[1] - diff(ylim) * .06
+  polygon(c(0.5, 0.5, xlim[2], xlim[2]),
+          c(lo.y, 0.5, 0.5, lo.y),
           col = "gray85",
           border = NA,
           xpd = TRUE)
-  text(x = x, 0 - diff(range(y)) * .03,
+  text(x = x,
+       mean(c(lo.y, .5)),
+       adj = c(.5, .5),
        labels = NAperColumn,
        col = ifelse(NAperColumn > 0, "red", "black"),
        xpd = TRUE)
