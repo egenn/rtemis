f07ea61c209de45c1f457aab07b143952f466204
Stathis Gennatas <gennatas@gmail.com>
2020-11-08 14:36:43 -0800

    init lots of maps

---
+++ R/dplot3.leaflet.R
@@ -0,0 +1,145 @@
+# dplot3.leaflet.R
+# ::rtemis::
+# 2020 Efstathios D. Gennatas rtemis.lambdamd.org
+
+#' Plot choropleth map using \strong{leaflet}
+#'
+#' @param dat data.frame where first column is county FIPS codes and second column is values to be
+#' plotted. County names can be provided on a third column and will appear as hover-over information
+#' along with values from column 2
+#' @param fillOpacity Float: Opacity for fill colors. Default = 1
+#' @param palette String: Color palette to use
+#' @param color.mapping String: "Numeric" or "Bin"
+#' @param col.lo Overlay color mapped to lowest value
+#' @param col.hi Overaly color mapped to highest value
+#' @param col.na Color mappes to NA values
+#' @param col.highlight Hover border color. Default = "#FE8A4F" (orange)
+#' @param col.interpolate String: "linear" or "spline"
+#' @param col.bins Integer: Number of color bins to create if \code{color.mapping = "Bin"}.
+#' Default = 21
+#' @param domain Limits for mapping colors to values. Default = NULL and set to range
+#' @param weight Float: Weight of county border lines. Default = .5
+#' @param color Color of county border lines. Default = "black"
+#' @param alpha Float: Overaly transparency. Default = 1
+#' @param bg.tile.provider Background tile (below overlay colors), one of \code{leaflet::providers}
+#' @param bg.tile.alpha Float: Background tile transparency. Default = .67
+#' @param fg.tile.provider Foreground tile (above overlay colors), one of \code{leaflet::providers}
+#' @param legend.position String: One of: "topright", "bottomright", "bottomleft", "topleft".
+#' Default = "topright"
+#' @param legend.alpha Float: Legend box transparency. Default = .8
+#' @param legend.title String: Defaults to \code{colnames(dat)[2]}
+#' @param init.lng Float: Center map around this longitude (in decimal form).
+#' Default = -98.54180833333334 (US geographic center)
+#' @param init.lat Float: Center map around this latitude (in decimal form).
+#' Default = 39.207413888888894 (US geographic center)
+#' @param init.zoom Integer: Initial zoom level (depends on device, i.e. window, size). Default = 3
+#' @param stroke Logical: If TRUE, draw polygon borders. Default = TRUE
+#' @author Efstathios D. Gennatas
+#' @export
+
+dplot3.leaflet <- function(dat,
+                           fillOpacity = 1,
+                           palette = NULL,
+                           color.mapping = c("Numeric", "Bin"),
+                           col.lo = "#0290EE",
+                           col.hi = "#FE4AA3",
+                           col.na = "#252525",
+                           col.highlight = "#FE8A4F",
+                           col.interpolate = c("linear", "spline"),
+                           col.bins = 21, # for color.mapping Bin
+                           domain = NULL,
+                           weight = .5,
+                           color = "black",
+                           alpha = 1,
+                           bg.tile.provider = leaflet::providers$Stamen.TonerBackground,
+                           bg.tile.alpha = .67,
+                           fg.tile.provider = leaflet::providers$Stamen.TonerLabels,
+                           # fg.tile.alpha = .67,
+                           legend.position = c("topright", "bottomright", "bottomleft", "topleft"),
+                           legend.alpha = .8,
+                           legend.title = NULL,
+                           init.lng = -98.54180833333334,
+                           init.lat = 39.207413888888894,
+                           init.zoom = 3,
+                           stroke = TRUE) {
+
+  # '- Arguments ====
+  color.mapping <- match.arg(color.mapping)
+  if (is.null(palette)) {
+    palette <- colorRamp(colors = c(col.lo, col.hi), interpolate = col.interpolate)
+  }
+  col.interpolate <- match.arg(col.interpolate)
+  legend.position <- match.arg(legend.position)
+  if (is.null(legend.title)) legend.title <- labelify(colnames(dat)[2])
+
+  # '- Data ====
+  counties <- geojsonio::geojson_read("https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json",
+                                      what = "sp")
+  fips <- if (is.numeric(dat[, 1])) sprintf("%05d", dat[, 1]) else dat[, 1]
+  # Match input county-level data
+  index <- match(counties$id, fips)
+  counties[["val"]] <- dat[index, 2]
+
+  # '- Colorscale ====
+  # if (is.null(domain)) domain <- counties$val
+  if (color.mapping == "Numeric") {
+    pal <- leaflet::colorNumeric(palette = palette,
+                                 domain = domain,
+                                 na.color = col.na,
+                                 alpha = TRUE)
+  } else {
+    pal <- leaflet::colorBin(palette = palette,
+                             domain = domain,
+                             na.color = col.na,
+                             bins = col.bins)
+  }
+
+  # '- Hover labels ====
+  if (ncol(dat) > 2) {
+    labels <- sprintf("<strong>%s</strong><br/>%g", dat[, 3], dat[, 2]) %>%
+      lapply(htmltools::HTML)
+  } else {
+    labels <- sprintf("%g", dat[, 2]) %>% lapply(htmltools::HTML)
+  }
+
+  # '- leaflet map ====
+  map <- leaflet::leaflet(counties) %>%
+    leaflet::addProviderTiles(provider = bg.tile.provider,
+                              options = leaflet::providerTileOptions(opacity = bg.tile.alpha)) %>%
+    leaflet::addMapPane("polygons", zIndex = 410) %>%
+    leaflet::addMapPane("tiles", zIndex = 420) %>%
+    leaflet::addPolygons(fillColor = ~pal(val),
+                         fillOpacity = fillOpacity,
+                         opacity = alpha,
+                         weight = weight,
+                         color = color,
+                         stroke = stroke,
+                         group = "overlay",
+                         options = leaflet::pathOptions(pane = "polygons"),
+                         highlight = leaflet::highlightOptions(
+                           weight = 2,
+                           color = col.highlight,
+                           bringToFront = TRUE),
+                         label = labels,
+                         labelOptions = leaflet::labelOptions(
+                           style = list("font-weight" = "normal", padding = "2px 2px"),
+                           textsize = "15px",
+                           direction = "auto")
+                         ) %>%
+    leaflet::addProviderTiles(provider = fg.tile.provider,
+                              options = leaflet::pathOptions(pane = "tiles")) %>%
+    leaflet::addLegend(position = legend.position,
+                       pal = pal,
+                       values = counties$val,
+                       opacity = legend.alpha,
+                       title = legend.title) %>%
+    leaflet::addLayersControl(overlayGroups = c("overlay")) %>%
+    leaflet::setView(lng = init.lng, lat = init.lat, zoom = init.zoom)
+  map
+
+}
+
+
+# "geographic center" of the United States:
+# latitude 39°12'26.686", longitude 98°32'30.506"
+# lat: 39.207413888888894, long: -98.54180833333334
---
+++ R/gplot3.map.R
@@ -0,0 +1,193 @@
+# gplot3.map
+# ::rtemis::
+# 2020 Efstathios D. Gennatas rtemis.lambdamd.org
+
+#' Plot US state or county choropleth map
+#'
+#' Plot US state or county choropleth map using \strong{ggplot2} based on
+#' \code{usmap::plot_usmap()}
+#'
+#' @param dat data.frame with 2 columns: fips, value
+#' @param regions Vector, character: "states", "state", "counties", "county"
+#' @param include Vector, character: Names of states or counties to include
+#' @param exclude Vector, character: Names of states or counties to exclude
+#' @param col.lo Color: Color mapped ot lowest negative values. Default = "#0290EE"
+#' @param col.mid Color: Color mapped to value \code{colorscale.midpoint}, only used if
+#' \code{colorscale.midpoint} is not NULL. If set to NA, \code{colorscale.midpoint} will be
+#' set to NULL. Default = "black".
+#' @param col.hi Color: Color mapped to highest positive values. Default = "#FE4AA3"
+#' @param col.na Color: Color mapped to NA values. Default = "gray40"
+#' @param colorscale.midpoint Float: Midpoint for colorscale. Default = 0
+#' @param colorbar.height FLoat: Colorbar height, will be used as
+#' \code{ggplot2::unit(colorbar.height, "npc")}
+#' @param colorbar.width FLoat: Colorbar width, will be used as
+#' \code{ggplot2::unit(colorbar.width, "npc")}
+#' @param border.width Float: Map border width. Default = .1
+#' @param main String: Main title. Default = NULL
+#' @param legend.title String: Legend title. Default = NULL
+#' @param theme \strong{ggplot2} theme to use. Default = \code{rt_gtheme_map()}
+#' @param labels Logical: If TRUE, label states. Default = FALSE
+#' @param col.labels Color for \code{labels}. Default = "gray50"
+#' @author Efstathios D. Gennatas
+#' @export
+
+gplot3.map <- function(dat,
+                       regions = c("states", "state", "counties", "county"),
+                       include = NULL,
+                       exclude = NULL,
+                       limits = NULL,
+                       trans = "identity",
+                       col.lo = "#0290EE",
+                       col.mid = "black",
+                       col.hi = "#FE4AA3",
+                       col.na = "grey40",
+                       colorscale.midpoint = 0,
+                       colorbar.height = .15,
+                       colorbar.width = .03,
+                       border.width = .1,
+                       main = NULL,
+                       legend.title = NULL,
+                       labelify = TRUE,
+                       theme = rt_gtheme_map(),
+                       labels = FALSE,
+                       col.labels = "gray50", ...) {
+
+  regions_ <- match.arg(regions)
+  colnames(dat)[1] <- "fips"
+  if (is.null(legend.title)) legend.title <- colnames(dat)[2]
+  if (is.na(col.mid)) colorscale.midpoint <- NULL
+
+  # geom_args ====
+  geom_args <- list(...)
+
+  if (is.null(geom_args[["colour"]]) & is.null(geom_args[["color"]])) {
+    geom_args[["color"]] <- "gray50"
+  }
+
+  if (is.null(geom_args[["size"]])) {
+    geom_args[["size"]] <- border.width
+  }
+
+  if (is.null(geom_args[["fill"]]) & nrow(dat) == 0) {
+    geom_args[["fill"]] <- "white"
+  } else if (!is.null(geom_args[["fill"]]) & nrow(dat) != 0) {
+    warning("`fill` setting is ignored when `dat` is provided. Use `fill` to color regions with solid color when no data is being displayed.")
+  }
+
+  # data ====
+  values <- colnames(dat)[2]
+  map_df <- usmap::map_with_data(dat,
+                                 values = values,
+                                 include = include,
+                                 exclude = exclude)
+  geom_args[["mapping"]] <- ggplot2::aes(x = x, y = y,
+                                         group = group,
+                                         fill = map_df[, values])
+
+  polygon_layer <- do.call(ggplot2::geom_polygon, geom_args)
+
+  # '- Labels ====
+  if (labels) {
+    centroidLabelsColClasses <- c("numeric", "numeric",
+                                  "character", "character", "character")
+
+    if (regions_ == "county" | regions_ == "counties") {
+      centroidLabelsColClasses <- c(centroidLabelsColClasses,
+                                    "character")
+    }
+
+    centroid_labels <- utils::read.csv(system.file("extdata",
+                                                   paste0("us_", regions_, "_centroids.csv"), package = "usmap"),
+                                       colClasses = centroidLabelsColClasses, stringsAsFactors = FALSE)
+
+    if (length(include) > 0) {
+      centroid_labels <- centroid_labels[centroid_labels$full %in%
+                                           include | centroid_labels$abbr %in% include |
+                                           centroid_labels$fips %in% include, ]
+    }
+
+    if (length(exclude) > 0) {
+      centroid_labels <- centroid_labels[!(centroid_labels$full %in%
+                                             exclude | centroid_labels$abbr %in% exclude |
+                                             centroid_labels$fips %in% exclude | substr(centroid_labels$fips,
+                                                                                        1, 2) %in% exclude), ]
+    }
+
+    if (regions_ == "county" | regions_ == "counties") {
+      label_layer <- ggplot2::geom_text(data = centroid_labels,
+                                        ggplot2::aes(x = x, y = y,
+                                                     label = sub(" County", "", county)),
+                                        color = col.labels)
+    } else {
+      label_layer <- ggplot2::geom_text(data = centroid_labels,
+                                        ggplot2::aes(x = x, y = y, label = abbr),
+                                        color = col.labels)
+    }
+  } else {
+    label_layer <- ggplot2::geom_blank()
+  }
+
+  # '- colorscale ====
+  colorscale <- if (is.null(colorscale.midpoint)) {
+    ggplot2::scale_fill_gradient(limits = limits,
+                                 oob = scales::squish,
+                                 trans = trans,
+                                 low = col.lo,
+                                 high = col.hi,
+                                 space = "Lab",
+                                 na.value = col.na,
+                                 guide = "colorbar",
+                                 aesthetics = "fill")
+  } else {
+    ggplot2::scale_fill_gradient2(limits = limits,
+                                  oob = scales::squish,
+                                  trans = trans,
+                                  low = col.lo,
+                                  mid = col.mid,
+                                  high = col.hi,
+                                  midpoint = colorscale.midpoint,
+                                  space = "Lab",
+                                  na.value = col.na,
+                                  guide = "colorbar",
+                                  aesthetics = "fill")
+  }
+
+
+  # '- colorbar ====
+  guide <- ggplot2::guides(fill = ggplot2::guide_colorbar(barheight = ggplot2::unit(colorbar.height, "npc"),
+                                                          barwidth = ggplot2::unit(colorbar.width, "npc"),
+                                                          ticks.colour = NA,
+                                                          ticks.linewidth = 1))
+
+  # '- ggplot ====
+  if (labelify) legend.title <- labelify(legend.title)
+  ggplot2::ggplot(data = map_df) +
+    polygon_layer + label_layer + ggplot2::coord_equal() + theme +
+    colorscale + guide + ggplot2::labs(fill = legend.title) +
+    ggplot2::ggtitle(main)
+
+} # rtemis::gplot3.map
+
+#' **ggplot2** map theme
+#'
+#' **ggplot2** map theme for use with \link{gplot3.map}
+#' based on \code{usmap:::theme_map()}
+#'
+#' @param base_size Float: Character \code{base_size.} Default = 9
+#' @param base_family String: Font family. Default = "Lato"
+#' @author Efstathios D. Gennatas
+#' @export
+
+rt_gtheme_map <- function(base_size = 9, base_family = "Lato") {
+
+  blank <- ggplot2::element_blank()
+  ggplot2::theme_bw(base_size = base_size, base_family = base_family) +
+    ggplot2::theme(axis.line = blank, axis.text = blank,
+                   axis.ticks = blank, axis.title = blank,
+                   panel.background = blank, panel.border = blank,
+                   panel.grid = blank, plot.background = blank,
+                   panel.spacing = ggplot2::unit(0, "lines"),
+                   legend.justification = c(0, 0),
+                   legend.position = "right")
+
+} # rtemis::rt_gtheme_map
