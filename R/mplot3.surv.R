df681e4ff9cda4627056aa57c6e0aa8fe4824d2f
egenn <gennatas@gmail.com>
2021-01-05 22:24:00 -0800

    cleanup and add error

--- R/mplot3.surv.R
+++ R/mplot3.surv.R
@@ -11,7 +11,7 @@
 #' @param lty Integer: Line type. Default = 1. See \code{par("lty")}
 #' @param lwd Float: Line width. Default = 2
 #' @param alpha Float: Alpha for lines. Default = 1
-#' @param normalize Logical: If TRUE: \link{drange} \code{x} to 0:100
+# @param normalize Logical: If TRUE: \link{drange} \code{x} to 0:100
 #' @param ... Additional arguments to pass to \link{mplot3.xy}
 #' @author Efstathios D. Gennatas
 #' @export
@@ -21,11 +21,14 @@ mplot3.surv <- function(x,
                         lwd = 2,
                         alpha = 1,
                         col = NULL,
-                        normalize = TRUE,
+                        # normalize = TRUE,
                         cex = 1.2,
                         xlab = "Time",
                         ylab = "Survival",
                         main = "Kaplan-Meier estimate with 95% CI",
+                        plot.error = TRUE,
+                        error.lty = 2,
+                        error.alpha = .5,
                         group.legend = NULL,
                         group.title = "",
                         group.names = NULL,
@@ -39,15 +42,14 @@ mplot3.surv <- function(x,
   if (class(x)[1] != "list") x <- list(x)
   # x <- lapply(1:length(x), function(i) as.numeric(as.matrix(x[[i]])[, 1]))
   # if (class(x)[[1]] != "Surv") stop("At least first object must be of type Survival")
-  for (i in 1:length(x)) {
+  for (i in seq(x)) {
     if (class(x[[i]]) != "Surv") {
       x[[i]] <- survival::Surv(x[[i]], rep(1, length(x[[i]])))
     }
   }
 
   # [ Kaplan-Meier Estimate ] ====
-  .survfit <- lapply(x, function(x) survival::survfit(x ~ 1))
-  lapply(1:length(x), function(i) survival::survfit(x[[i]] ~ 1))
+  .survfit <- lapply(x, function(i) survival::survfit(i ~ 1))
 
   # # [ Normalize ] ====
   # if (normalize) {
@@ -55,7 +57,9 @@ mplot3.surv <- function(x,
   # }
 
   # [ LIMITS ] ====
-  xlim <- c(0, max(.survfit[[1]]$time))
+  xl <- lapply(.survfit, function(i) i$time)
+  xlim <- range(unlist(xl))
+  yl <- lapply(.survfit, function(i) i$surv)
 
   # [ PLOT ] ====
   if (exists("rtpar", envir = rtenv)) par.reset <- FALSE
@@ -64,28 +68,25 @@ mplot3.surv <- function(x,
 
   cols <- if (is.null(col)) ucsfCol else col
 
-  # with pointwise errors:
-  mplot3.xy(x = .survfit[[1]]$time,
-            y = list(.survfit[[1]]$surv, Upper = .survfit[[1]]$upper, Lower = .survfit[[1]]$lower),
-            ylim = c(0, 1), xlim = xlim,
-            type = 's', lwd = 2, lty = c(1, 3, 3),
-            marker.col = c(cols[[1]], colorAdjust(cols[[1]], .66), colorAdjust(cols[[1]], .66)),
+
+  mplot3.xy(x = xl,
+            y = yl,
+            ylim = c(0, 1),
+            xlim = xlim,
+            type = 's', lwd = 2, lty = lty,
+            marker.col = col,
             line.alpha = 1,
             main = main,
             xlab = "Time", ylab = "Survival",
             group.legend = FALSE, zerolines = FALSE, par.reset = FALSE, ...)
-  if (length(x) > 1) {
-    for (i in 2:length(x)) {
-      mplot3.xy(x = .survfit[[i]]$time,
-                y = list(.survfit[[i]]$surv, Upper = .survfit[[i]]$upper, Lower = .survfit[[i]]$lower),
-                ylim = c(0, 1), xlim = xlim,
-                type = 's', lwd = 2, lty = c(1, 3, 3),
-                marker.col = c(cols[[i]], colorAdjust(cols[[i]], .66), colorAdjust(cols[[i]], .66)),
-                line.alpha = 1,
-                main = "",
-                xlab = "Time", ylab = "Survival Function",
-                group.legend = FALSE, zerolines = FALSE, par.reset = FALSE,
-                axes.visible = FALSE, new = TRUE, ...)
+
+  # pointwise errors
+  if (plot.error) {
+    for (i in seq(xl)) {
+      lines(.survfit[[i]]$time, .survfit[[i]]$upper,
+            lty = error.lty, col = colorAdjust(col[[i]], error.alpha))
+      lines(.survfit[[i]]$time, .survfit[[i]]$lower,
+            lty = error.lty, col = colorAdjust(col[[i]], error.alpha))
     }
   }
 
@@ -114,5 +115,3 @@ mplot3.surv <- function(x,
   }
 
 } # rtemis::mplot3.surv
-
-
