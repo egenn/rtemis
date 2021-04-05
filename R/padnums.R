57abfc41cd0d9fdd610a5df59533d6d1776d193c
egenn <gennatas@gmail.com>
2021-04-05 16:00:24 -0700

    init

---
+++ R/padnums.R
@@ -0,0 +1,12 @@
+# padnums.R
+# ::rtemis::
+# 2021 E.D. Gennatas lambdamd.org
+
+padnums <- function(x, pad = "X") {
+  for (i in seq_along(x)) {
+    if (grepl("[[:digit:]]", substr(x[i], 1, 1))) {
+      x[i] <- paste0(pad, x[i])
+    }
+  }
+  x
+}
