b658924703fd945a7da21865cd17c93d7ceb94b4
egenn <gennatas@gmail.com>
2021-04-07 22:20:47 -0700

    correctly pad low vals

--- R/rtPalette.R
+++ R/rtPalette.R
@@ -1458,7 +1458,7 @@ rtemis_palette <- function(n,
 
 #' Convert R color to hexadecimal code
 #'
-#' Convert any color that R understands into the corresponding hexadecimal code
+#' Convert a color that R understands into the corresponding hexadecimal code
 #'
 #' @param color Color(s) that R understands
 #' @author E.D. Gennatas
@@ -1470,7 +1470,12 @@ col2hex <- function(color) {
 
   .rgb <- col2rgb(color)
   sapply(seq(color), function(i) {
-    paste0("#", as.hexmode(.rgb[1, i]), as.hexmode(.rgb[2, i]), as.hexmode(.rgb[3, i]))
+    paste0("#", paste0(sprintf("%02s",
+           c(as.character(as.hexmode(.rgb[1, i])),
+             as.character(as.hexmode(.rgb[2, i])),
+             as.character(as.hexmode(.rgb[3, i])))
+           ), collapse = ""
+           ))
   })
 
-}
+} # rtemis::col2hex
