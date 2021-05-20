cd4f254844468f9d0e12257cb64a164ba9ef7ac4
egenn <gennatas@gmail.com>
2021-05-20 15:11:56 -0400

    added toLower [ci skip]

--- R/labelify.R
+++ R/labelify.R
@@ -7,7 +7,11 @@
 #' @param x Character: Input
 #' @param underscoresToSpaces Logical: If TRUE, convert underscores to spaces. Default = TRUE
 #' @param dotsToSpaces Logical: If TRUE, convert dots to spaces. Default = TRUE
-#' @param toTitleCase Logical: If TRUE, convert to Title Case. Default = TRUE
+#' @param toLower Logical: If TRUE, convert to lowercase (precedes \code{toTitleCase}).
+#' Default = FALSE (Good for getting all-caps words converted to title case, bad for abbreviations
+#' you want to keep all-caps)
+#' @param toTitleCase Logical: If TRUE, convert to Title Case. Default = TRUE (This does not change
+#' all-caps words, set \code{toLower} to TRUE if desired)
 #' @param capitalize.strings String, vector: Always capitalize these strings, if present. Default = "id"
 #' @param stringsToSpaces String, vector: Replace these strings with spaces. Escape as needed for \code{gsub}.
 #' Default = "\\$", which formats common input of the type \code{data.frame$variable}
@@ -18,6 +22,7 @@
 labelify <- function(x,
                      underscoresToSpaces = TRUE,
                      dotsToSpaces = TRUE,
+                     toLower = FALSE,
                      toTitleCase = TRUE,
                      capitalize.strings = c("id"),
                      stringsToSpaces = c("\\$")) {
@@ -28,6 +33,7 @@ labelify <- function(x,
   }
   if (underscoresToSpaces) xf <- gsub("_", " ", xf)
   if (dotsToSpaces) xf <- gsub("\\.", " ", xf)
+  if (toLower) xf <- tolower(xf)
   if (toTitleCase) xf <- tools::toTitleCase(xf)
   for (i in capitalize.strings) {
     xf <- gsub(tools::toTitleCase(i), toupper(i), xf)
