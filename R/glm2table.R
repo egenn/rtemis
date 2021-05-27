26b231056d1e19ebed91f9d8d095f0ccaa86e626
egenn <gennatas@gmail.com>
2021-05-26 21:05:18 -0400

    init [ci skip]

---
+++ R/glm2table.R
@@ -0,0 +1,36 @@
+# glmlist2table.R
+# ::rtemis::
+# 2021 E.D. Gennatas lambdamd.org
+
+#' Collect summary tablew from list of massGLMs with same predictors, different outcome
+#'
+#' @param x list of \link{glm} models
+#' @param xnames Character, vector: names of models, i.e. normally name of
+#' @return \code{data.table} with glm summaries
+#' @author E.D. Gennatas
+#' @export
+#'
+
+glm2table <- function(x, xnames = NULL) {
+
+  if (is.null(xnames)) {
+    xnames <- if (!is.null(names(x))) {
+      names(x)
+    } else {
+      paste0("Model_", seq_along(x))
+    }
+  }
+
+  data.table(Variable = xnames,
+             do.call(rbind,
+                     c(lapply(x, \(y) {
+                       out <- t(coef(summary(y))[-1, ])
+                       varnames <- gsub(".*\\$", "", colnames(out))
+                       parnames <- c("Coefficient", "SE", "t_value", "p_value")
+                       out <- c(out)
+                       names(out) <- c(outer(parnames, varnames, paste))
+                       out
+                     })))
+  )
+
+} # rtemis::glm2table
