08c4114e18459066b3eae8e93a22af6e09e716ef
egenn <gennatas@gmail.com>
2021-06-17 00:58:59 -0400

    init onehot2factor

---
+++ R/onehot2factor.R
@@ -0,0 +1,26 @@
+# onehot2factor
+# ::rtemis::
+# 2021 E.D. Gennatas lambdamd.org
+
+#' Convert one-hot encoded matrix to factor
+#'
+#' @param x one-hot encoded matrix or data.frame
+#' @param labels Character vector of level names. Default = \code{colnames(x)}
+#' @author E.D. Gennatas
+#' @export
+#' @examples
+#' \dontrun{
+#' x <- data.frame(matrix(F, 10, 3))
+#' colnames(x) <- c("Dx1", "Dx2", "Dx3")
+#' x$Dx1[1:3] <- x$Dx2[4:6] <- x$Dx3[7:10] <- T
+#' onehot2factor(x)
+#' }
+
+onehot2factor <- function(x, labels = colnames(x)) {
+  if (any(rowSums(x) > 1)) stop("Input must be one-hot encoded.")
+  out <- factor(NROW(x), levels = labels)
+  for (i in seq_along(labels)) {
+    out[x[, i] == 1] <- labels[i]
+  }
+  out
+}
