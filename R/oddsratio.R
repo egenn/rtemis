45c31661df6ecb90e408b4ee5035725d754ab45b
egenn <gennatas@gmail.com>
2021-09-05 20:04:42 -0700

    init

---
+++ R/oddsratio.R
@@ -0,0 +1,36 @@
+# oddsratio.R
+# ::rtemis::
+# E.D. Gennatas lambdamd.org
+
+#' Calculate odds ratio for a 2x2 contingency table
+#'
+#' @param x 2x2 contingency table (created with \code{table(x, y)}, where \code{x} and \code{y}
+#' are factors with the first level being the control / unaffected / negative)
+#'
+#' @export
+
+oddsratio <- function(x, verbose = TRUE) {
+
+  stopifnot(inherits(x, "table"))
+  stopifnot(dim(x) == c(2, 2))
+
+  odds1 <- x[2, 1]/sum(x[, 1])
+  odds2 <- x[2, 2]/sum(x[, 2])
+  out <- data.frame(matrix(c(x), 2))
+  colnames(out) <- colnames(x)
+  rownames(out) <- rownames(x)
+  out <- rbind(out, c(odds1, odds2))
+  rownames(out) <- c(rownames(x), "Odds")
+  out
+  or <- odds2/odds1
+  attr(out, "OddsRatio") <- or
+
+  if (verbose) {
+    print(out)
+    cat("Odds Ratio:", rtHighlight(ddSci(or)), "\n")
+  }
+
+  out
+
+}
+
