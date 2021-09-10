68527b74444c45affaaebf2e206528b81c8a2577
egenn <gennatas@gmail.com>
2021-09-10 02:09:30 -0700

    init sge_submit

---
+++ R/sge_submit.R
@@ -0,0 +1,149 @@
+# sge_submit.R
+# ::rtemis::
+# E.D. Gennatas lambdamd.org
+
+#' Submit expression to SGE grid
+#'
+#' @param expr R expression
+#' @param obj_names Character vector: Names of objects to copy to cluster R session
+#' @param packages Character vector: Names of packages to load in cluster R session
+#' @param queue Characer: Name of SGE queue to submit to
+#' @param n_threads Integer: Number of threads to request from scheduler
+#' @param sge_out Character: Path to directory to write standard out message files
+#' @param sge_error Character: Path to directory to write error message files
+#' @param sge_env Character: Shell environment for script to be submitted to SGE
+#' @param sge_opts Character: SGE options that will be written in shell script. Default = "#$ -cwd"
+#' @param system_command Character: system command to be run by shell script before executing R code.
+#' For example a command that export the R executable to use
+#' @param h_rt Character: Max time to request. Default = "00:25:00", i.e. 25 minutes
+#' @param mem_free Character: Amount of memory to request from the scheduler
+#' @param temp_dir Character: Temporary directory that is accessible to all execution nodes.
+#' Default = \code{file.path(getwd(), ".sge_tempdir")}
+#' @param verbose Logical: If TRUE, print messages to console. Default = TRUE
+#' @param trace Integer: If > 0 print diagnostic messages to console. Default = 1
+#' @author E.D. Gennatas
+#' @export
+
+sge_submit <- function(expr,
+                       obj_names = NULL,
+                       packages = NULL,
+                       queue = NULL,
+                       n_threads = 4,
+                       sge_out = file.path(getwd(), "./sge_out"),
+                       sge_error = sge_out,
+                       sge_env = "#! /usr/bin/env bash",
+                       sge_opts = "#$ -cwd",
+                       system_command = NULL,
+                       h_rt = "00:25:00",
+                       mem_free = NULL,
+                       temp_dir = file.path(getwd(), ".sge_tempdir"),
+                       verbose = TRUE,
+                       trace = 1) {
+
+  expr <- as.character(as.expression(substitute(expr)))
+
+  # Save obj_names to temp
+
+  if (!is.null(obj_names)) {
+    .temp <- file.path(temp_dir, "mango")
+    do.call(save, list(list = obj_names,
+                       file = .temp))
+    if (trace > 0) {
+      msg("Temp file set to", .temp)
+      msg("Objects written to temp file:", paste(obj_names, collapse = ", "))
+    }
+  }
+
+  # Create temp_dir
+  if (!dir.exists(temp_dir)) {
+    dir.create(temp_dir)
+    stopifnot(dir.exists(temp_dir))
+    if (trace > 0) msg("Created temp_dir", temp_dir)
+  }
+
+  # sge_out and sge_error
+  if (!dir.exists(sge_out)) {
+    dir.create(sge_out)
+    if (trace > 0) msg("Created sge_out", sge_out)
+  }
+  if (!dir.exists(sge_error)) {
+    dir.create(sge_error)
+    if (trace > 0) msg("Created sge_error", sge_error)
+  }
+  if (trace > 0) {
+    msg("sge_out set to:", sge_out)
+    msg("sge_error set to:", sge_error)
+  }
+
+  # Write .R file to temp_dir
+  Rfilepath <- file.path(temp_dir, "sge_submit.R")
+  if (trace > 0) msg("Rfilepath set to", Rfilepath)
+
+  # init file
+  cat(paste("# rtemis sge_submit", date()), file = Rfilepath)
+
+  # Load packages
+  if (!is.null(packages)) {
+    cat(sapply(packages, function(p) paste0("library(", p, ")")),
+        file = Rfilepath, append = TRUE)
+  }
+
+  # Diag
+  cat("rtemis::msg('Running on', system('uname -n'), 'as', system('whoami'))",
+      Rfilepath, append = TRUE)
+
+  # Load data
+  if (!is.null(obj_names)) {
+    cat(paste0("load('", .temp, "')"), file = Rfilepath, append = TRUE)
+    msg("Objects available:", paste(ls(), collapse = ", "), "\n")
+  }
+
+  # Expression
+  cat(as.character(expr), file = Rfilepath, append = TRUE)
+  stopifnot(file.exists(Rfilepath))
+
+  # Write .sh file to temp_dir
+  shfilepath <- file.path(temp_dir, "sge_submit.sh")
+  if (trace > 0) msg("shfile set to:", shfilepath)
+
+  cat(sge_env, file = shfilepath)
+  cat(paste0(sge_opts, "\n"), file = shfilepath, append = TRUE)
+  if (!is.null(system_command)) {
+    cat(system_command, file = shfilepath, append = TRUE)
+  }
+  cat(paste("Rscript", Rfilepath), file = shfilepath, append = TRUE)
+
+  stopifnot(file.exists(shfilepath))
+
+  # Submit .sh to grid
+  qsub <- paste(
+    "qsub -pe smp", n_threads,
+    "-o", sge_out,
+    "-e", sge_error)
+  if (!is.null(queue)) {
+    qsub <- paste(qsub, "-q", queue)
+  }
+  if (!is.null(h_rt)) {
+    qsub <- paste(qsub, paste0("-l h_rt=", h_rt))
+  }
+  if (!is.null(mem_free)) {
+    qsub <- paste(qsub, paste0("-l mem_free=", mem_free))
+  }
+  qsub <- paste(qsub, shfilepath)
+
+  system(qsub)
+
+}
+
+
+#' SGE qstat
+#'
+#' Run SGE qstat
+#'
+#' alias for \code{system("qstat")}
+#'
+#' @export
+
+qstat <- function() {
+  system("qstat")
+}
