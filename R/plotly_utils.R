d517973e27f7b24312df7a6f5e286e3bf0536847
Stathis <gennatas@gmail.com>
2022-01-13 03:31:10 -0500

    init

--- R/plotly_utils.R
+++ R/plotly_utils.R
@@ -29,3 +29,29 @@ plotly_vline <- function(x,
          SIMPLIFY = FALSE)
   
 }
+
+plotly_hline1 <- function(y, 
+                          color = "#F48024",
+                          width = 1,
+                          dash = "dot") {
+  list(
+    type = "line",
+    x0 = 0, x1 = 1,
+    y0 = y, y1 = y, xref = "paper",
+    line = list(color = color,
+                width = width,
+                dash = dash)
+  )
+}
+
+plotly_hline <- function(y, 
+                         color = "#F48024",
+                         width = 1,
+                         dash = "dot") {
+  color <- recycle(color, y)
+  width <- recycle(width, y)
+  dash <- recycle(dash, y)
+  mapply(plotly_hline1, y, color = color, width = width, dash = dash,
+         SIMPLIFY = FALSE)
+  
+}
