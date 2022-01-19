bfcd43af616fe2f2f230e8cf893f743be8b32512
Stathis <gennatas@gmail.com>
2022-01-19 00:01:18 -0500

    cleanup [ci skip]

---
+++ R/col2hex.R
@@ -0,0 +1,23 @@
+#' Convert R color to hexadecimal code
+#'
+#' Convert a color that R understands into the corresponding hexadecimal code
+#'
+#' @param color Color(s) that R understands
+#' @author E.D. Gennatas
+#' @export
+#' @examples
+#' col2hex(c("gray50", "skyblue"))
+
+col2hex <- function(color) {
+
+  .rgb <- col2rgb(color)
+  sapply(seq(color), function(i) {
+    paste0("#", paste0(sprintf("%02s",
+           c(as.character(as.hexmode(.rgb[1, i])),
+             as.character(as.hexmode(.rgb[2, i])),
+             as.character(as.hexmode(.rgb[3, i])))
+           ), collapse = ""
+           ))
+  })
+
+} # rtemis::col2hex
