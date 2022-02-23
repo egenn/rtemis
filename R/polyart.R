9075de6002886a050b7d097f3784dd8533b3a9df
Stathis <gennatas@gmail.com>
2022-02-23 05:45:58 -0800

    wip

--- R/polyart.R
+++ R/polyart.R
@@ -104,7 +104,7 @@ polyshadow1 <- function(k_h = 20, k_v = 20,
                         text.x = 95,
                         text.y = 5, 
                         text.adj = c(1, 0),
-                        col.text = "#ffffff",
+                        text.col = "#ffffff",
                         text.cex = 2,
                         font.family = "") {
   
@@ -144,7 +144,7 @@ polyshadow1 <- function(k_h = 20, k_v = 20,
          y = text.y, 
          labels = text, 
          adj = text.adj,
-         col = col.text,
+         col = text.col,
          cex = text.cex,
          family = font.family)
   }
@@ -164,7 +164,7 @@ polyshadow<- function(k_h = 20, k_v = 20,
                       text.x = 95,
                       text.y = 5, 
                       text.adj = c(1, 0),
-                      col.text = "#ffffff",
+                      text.col = "#ffffff",
                       text.cex = 2,
                       font.family = "",
                       seed = NULL) {
@@ -186,8 +186,10 @@ polyshadow<- function(k_h = 20, k_v = 20,
   
   
   # Plot ====
-  par(bg = bg, mai = rep(0, 4), mar = rep(0, 4), xaxs = "i", yaxs = "i")
-  plot(NULL, xlim = c(0, 100), ylim = c(0, 100))
+  par(bg = bg, mai = rep(0, 4), mar = rep(0, 4), 
+      xaxs = "i", yaxs = "i")
+  plot(NULL, xlim = c(0, 100), ylim = c(0, 100),
+       axes = FALSE, ann = FALSE)
   for (i in seq(k_v)) {
     for (j in seq(k_h)) {
       ind <- if (color.progression == "product") i*j else j + k_h*(i - 1)
@@ -216,7 +218,7 @@ polyshadow<- function(k_h = 20, k_v = 20,
          y = text.y, 
          labels = text, 
          adj = text.adj,
-         col = col.text,
+         col = text.col,
          cex = text.cex,
          family = font.family)
   }
@@ -226,29 +228,41 @@ polyshadow<- function(k_h = 20, k_v = 20,
 # polyshadow(50, 50, jsd = 5,
 #            text = "rtemis", 
 #            col_lo = "#00ffff", col_hi = "#ff00ff",
-#            text.cex = 2, col.text = "#ffffffef",
+#            text.cex = 2, text.col = "#ffffffef",
 #            text.x = 95, text.adj = c(1, 0))
 
 polyshadow2 <- function(k_h = 20, k_v = 20,
                         jsd = 1,
-                        shadow = seq(.98, .92, length.out = k_h*k_v),
+                        shadow = 1, # seq(.98, .92, length.out = k_h*k_v)
                         col_lo = "#00ffff", 
                         col_mid = NULL,
                         col_hi = "#ff00ff",
-                        color.progression = c("product", "counter"),
+                        space = c("rgb", "Lab"),
+                        alpha = TRUE,
+                        color.progression = c("mean", "product", "counter"),
                         shadow.direction = 1,
+                        border.alpha = 1,
                         poly.border = NA,
-                        bg = "#121212",
+                        bg = "#000000",
                         text = NULL,
-                        text.x = 95,
-                        text.y = 5, 
+                        text.x = 97,
+                        text.y = 3, 
                         text.adj = c(1, 0),
-                        col.text = "#ffffff",
+                        text.col = "#ffffff80",
                         text.cex = 2,
+                        text.srt = 0,
                         font.family = "",
-                        seed = NULL) {
+                        xpd = FALSE,
+                        seed = NULL,
+                        filename = NULL,
+                        pdf.width = 6,
+                        pdf.height = 6,
+                        trace = 0,
+                        par.reset = TRUE) {
   
   color.progression <- match.arg(color.progression)
+  # space <- match.arg(space)
+  shadow <- recycle(shadow, seq(k_h*k_v))
   
   if (!is.null(seed)) set.seed(seed)
   x <- matrix(rep(seq(0, 100, length.out = k_h + 1), k_v + 1), k_v + 1, byrow = TRUE)
@@ -261,34 +275,74 @@ polyshadow2 <- function(k_h = 20, k_v = 20,
     y[-c(1, k_v + 1), i] <- y[-c(1, k_v + 1), i] + rnorm(k_v - 1, sd = jsd)
   }
   
-  .col <- colorRampPalette(c(col_lo, col_mid, col_hi))(k_h * k_v)
+  .col <- colorRampPalette(c(col_lo, col_mid, col_hi), 
+                           # space = space,
+                           alpha = alpha)(k_h * k_v)
   
   
   # Plot ====
-  par(bg = bg, mai = rep(0, 4), mar = rep(0, 4), xaxs = "i", yaxs = "i")
-  plot(NULL, xlim = c(0, 100), ylim = c(0, 100))
+  if (!is.null(filename)) pdf(filename, width = pdf.width, height = pdf.height)
+  
+  if (par.reset) {
+    par.orig <- par(no.readonly = TRUE)
+    on.exit(par(par.orig))
+  }
+  par(bg = bg, mai = rep(0, 4), mar = rep(0, 4), 
+      xaxs = "i", yaxs = "i",
+      xpd = xpd)
+  plot(NULL, xlim = c(0, 100), ylim = c(0, 100),
+       axes = FALSE, ann = FALSE, xpd = xpd)
   for (i in seq(k_v)) {
     for (j in seq(k_h)) {
-      ind <- if (color.progression == "product") i*j else j + k_h*(i - 1)
-      if (shadow == 1) {
-        polygon(x = c(x[i], x[i + 1], x[i + 1], x[i]),
-                y = c(y[j], y[j], y[j + 1], y[j + 1]),
-                col = .col[i*j], border = NA)
+      # ind <- if (color.progression == "product") i*j else j + k_h*(i - 1)
+      ind <- switch (color.progression,
+        product = i*j,
+        counter = j + k_h*(i - 1),
+        # mean = round((.5*(i+j) / .5*(k_h+k_v)) * k_h*k_v)
+        mean = round( ((.5*(i+j)) / (.5*(k_h+k_v))) * k_h*k_v )
+      )
+      if (trace > 0) cat("i=", i, "; j=", j, "; ind=", ind, "\n")
+      if (all(shadow == 1)) {
+        if (border.alpha == 1) {
+          polygon(x = c(x[i, j], x[i+1, j], x[i+1, j+1], x[i, j+1]),
+                  y = c(y[i, j], y[i+1, j], y[i+1, j+1], y[i, j+1]),
+                  col = .col[ind], 
+                  border = poly.border,
+                  xpd = xpd)
+        } else {
+          col <- adjustcolor(.col[ind], 
+                             if (x[i, j] == 0 | x[i+1, j+1] == 100 | y[i, j] == 0 | y[i+1, j+1] == 100) 
+                               border.alpha else 1)
+          polygon(x = c(x[i, j], x[i+1, j], x[i+1, j+1], x[i, j+1]),
+                  y = c(y[i, j], y[i+1, j], y[i+1, j+1], y[i, j+1]),
+                  col = col, 
+                  border = poly.border,
+                  xpd = xpd)
+        }
+        
       } else {
         if (shadow.direction == 1) {
           polygon(x = c(x[i, j], x[i+1, j], x[i+1, j+1]),
                   y = c(y[i, j], y[i+1, j], y[i+1, j+1]),
-                  col = .col[ind], border = poly.border)
+                  col = .col[ind], 
+                  border = poly.border,
+                  xpd = xpd)
           polygon(x = c(x[i, j], x[i+1, j+1], x[i, j+1]),
                   y = c(y[i, j], y[i+1, j+1], y[i, j+1]),
-                  col = adjustcolor(.col[ind], shadow[ind]), border = poly.border)
+                  col = adjustcolor(.col[ind], shadow[ind]), 
+                  border = poly.border,
+                  xpd = xpd)
         } else {
           polygon(x = c(x[i, j], x[i+1, j], x[i, j+1]),
                   y = c(y[i, j], y[i+1, j], y[i, j+1]),
-                  col = adjustcolor(.col[ind], shadow[ind]), border = poly.border)
+                  col = adjustcolor(.col[ind], shadow[ind]), 
+                  border = poly.border,
+                  xpd = xpd)
           polygon(x = c(x[i+1, j], x[i+1, j+1], x[i, j+1]),
                   y = c(y[i+1, j], y[i+1, j+1], y[i, j+1]),
-                  col = .col[ind], border = poly.border)
+                  col = .col[ind], 
+                  border = poly.border,
+                  xpd = xpd)
         }
       }
     }
@@ -300,11 +354,19 @@ polyshadow2 <- function(k_h = 20, k_v = 20,
          y = text.y, 
          labels = text, 
          adj = text.adj,
-         col = col.text,
+         col = text.col,
          cex = text.cex,
-         family = font.family)
+         family = font.family,
+         srt = text.srt)
   }
+  
+  if (!is.null(filename)) dev.off()
   
 } # rtemis::polyshadow
 
-
+xy2v <- function(x, y) {
+  # 0, 0 = 0
+  # .5, .5 = .5
+  # 1, 1 = 1
+  mean(c(x, y))
+}
