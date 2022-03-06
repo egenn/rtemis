111e65cdf18b94702c3515f2e2692a4fbd0b5d5c
Stathis <gennatas@gmail.com>
2022-03-06 13:54:46 -0800

    init revfactorlevels

---
+++ R/revfactorlevels.R
@@ -0,0 +1,17 @@
+# revfactorlevels.R
+# ::rtemis::
+# 2022 E.D. Gennatas lambdamd.org
+
+#' Reverse factor level order
+#' 
+#' @param x factor
+#' @export 
+#' @author E.D. Gennatas
+
+revfactorlevels <- function(x) {
+    if (is.factor(x)) {
+        factor(x, levels = rev(levels(x)))
+    } else {
+        x
+    }
+} # rtemis::revfactorlevels
