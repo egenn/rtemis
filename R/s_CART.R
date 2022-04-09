659834f86363aac47e7d7b5e9974d74e9ca07334
Stathis <gennatas@gmail.com>
2022-04-08 23:05:56 -0700

    cleanup

--- R/s_CART.R
+++ R/s_CART.R
@@ -58,7 +58,7 @@ s_CART <- function(x, y = NULL,
                    method = "auto",
                    parms = NULL,
                    minsplit = 2,
-                   minbucket = round(minsplit/3),
+                   minbucket = round(minsplit / 3),
                    cp = 0.01,
                    maxdepth = 20,
                    maxcompete = 0,
@@ -88,266 +88,282 @@ s_CART <- function(x, y = NULL,
                    outdir = NULL,
                    save.mod = ifelse(!is.null(outdir), TRUE, FALSE),
                    rtModLog = NULL) {
+    tree.depth <- getFromNamespace("tree.depth", "rpart")
 
-  tree.depth <- getFromNamespace("tree.depth", "rpart")
-
-  # Intro ====
-  if (missing(x)) {
-    print(args(s_CART))
-    return(invisible(9))
-  }
-  if (!is.null(outdir)) outdir <- paste0(normalizePath(outdir, mustWork = FALSE), "/")
-  logFile <- if (!is.null(outdir)) {
-    paste0(outdir, sys.calls()[[1]][[1]], ".", format(Sys.time(), "%Y%m%d.%H%M%S"), ".log")
-  } else {
-    NULL
-  }
-  start.time <- intro(verbose = verbose, logFile = logFile)
-  mod.name <- "CART"
-
-  # Dependencies ====
-  dependency_check("rpart")
-
-  # Arguments ====
-  if (is.null(y) & NCOL(x) < 2) {
-    print(args(s_CART))
-    stop("y is missing")
-  }
-  if (is.null(x.name)) x.name <- getName(x, "x")
-  if (is.null(y.name)) y.name <- getName(y, "y")
-  if (!verbose) print.plot <- FALSE
-  verbose <- verbose | !is.null(logFile)
-  if (save.mod & is.null(outdir)) outdir <- paste0("./s.", mod.name, "/")
-  if (!is.null(outdir)) outdir <- paste0(normalizePath(outdir, mustWork = FALSE), "/")
-  control <- list(minsplit = minsplit,
-                  minbucket = minbucket,
-                  cp = cp,
-                  maxcompete = maxcompete,
-                  maxsurrogate = maxsurrogate,
-                  usesurrogate = usesurrogate,
-                  surrogatestyle = surrogatestyle,
-                  maxdepth = maxdepth,
-                  xval = xval)
-  grid.search.type <- match.arg(grid.search.type)
-
-  # Data ====
-  dt <- dataPrepare(x, y, x.test, y.test,
-                    ipw = ipw,
-                    ipw.type = ipw.type,
-                    upsample = upsample,
-                    downsample = downsample,
-                    resample.seed = resample.seed,
-                    verbose = verbose)
-  x <- dt$x
-  y <- dt$y
-  x.test <- dt$x.test
-  y.test <- dt$y.test
-  xnames <- dt$xnames
-  type <- dt$type
-  .weights <- if (is.null(weights) & ipw) dt$weights else weights
-  x0 <- if (upsample | downsample) dt$x0 else x # x0, y0 are passed to gridSearchLearn
-  y0 <- if (upsample | downsample) dt$y0 else y
-  if (verbose) dataSummary(x, y, x.test, y.test, type)
-  if (type != "Survival") df.train <- data.frame(y = y, x)
-  if (method == "auto") {
-    if (type == "Regression") {
-      method <- "anova"
-      if (is.null(metric)) metric <- "MSE"
-      if (is.null(maximize)) maximize <- FALSE
-    } else if (type == "Classification") {
-      method <- "class"
-      if (is.null(metric)) metric <- "Balanced Accuracy"
-      if (is.null(maximize)) maximize <- TRUE
-    } else if (type == "Survival") {
-      method <- "exp"
-      if (is.null(metric)) metric <- "Concordance"
-      if (is.null(maximize)) maximize <- TRUE
+    # Intro ====
+    if (missing(x)) {
+        print(args(s_CART))
+        return(invisible(9))
     }
-  }
-  if (is.null(cost)) cost <- rep(1, NCOL(x))
-  if (print.plot) {
-    if (is.null(plot.fitted)) plot.fitted <- if (is.null(y.test)) TRUE else FALSE
-    if (is.null(plot.predicted)) plot.predicted <- if (!is.null(y.test)) TRUE else FALSE
-  } else {
-    plot.fitted <- plot.predicted <- FALSE
-  }
-  if (is.null(metric)) {
-    if (type == "Classification") {
-      metric <- "Balanced Accuracy"
-    } else if (type == "Regression") {
-      metric <- "MSE"
+    if (!is.null(outdir)) outdir <- paste0(normalizePath(outdir, mustWork = FALSE), "/")
+    logFile <- if (!is.null(outdir)) {
+        paste0(outdir, sys.calls()[[1]][[1]], ".", format(Sys.time(), "%Y%m%d.%H%M%S"), ".log")
     } else {
-      metric <- "Concordance"
+        NULL
     }
-  }
-  if (type == "Classification") n.classes <- length(levels(y))
+    start.time <- intro(verbose = verbose, logFile = logFile)
+    mod.name <- "CART"
 
-  # Formula ====
-  features <- paste(xnames, collapse = " + ")
-  .formula <- as.formula(paste0(y.name, " ~ ", features))
+    # Dependencies ====
+    dependency_check("rpart")
 
-  # Grid Search for prune.cp ====
-  if (gridCheck(maxdepth, minsplit, minbucket, cp, prune.cp)) {
-    gs <- gridSearchLearn(x0, y0,
-                          mod = mod.name,
-                          resample.rtset = grid.resample.rtset,
-                          grid.params = list(maxdepth = maxdepth,
-                                             minsplit = minsplit,
-                                             minbucket = minbucket,
-                                             cp = cp,
-                                             prune.cp = prune.cp),
-                          fixed.params = list(method = method,
-                                              model = model,
-                                              maxcompete = maxcompete,
-                                              maxsurrogate = maxsurrogate,
-                                              usesurrogate = usesurrogate,
-                                              surrogatestyle = surrogatestyle,
-                                              xval = xval,
-                                              cost = cost,
-                                              na.action = na.action,
-                                              ipw = ipw,
-                                              ipw.type = ipw.type,
-                                              upsample = upsample,
-                                              downsample = downsample,
-                                              resample.seed = resample.seed),
-                          search.type = grid.search.type,
-                          randomized.p = grid.randomized.p,
-                          weights = weights,
-                          metric = metric,
-                          maximize = maximize,
-                          verbose = grid.verbose,
-                          n.cores = n.cores)
-    maxdepth <- gs$best.tune$maxdepth
-    minsplit <- gs$best.tune$minsplit
-    minbucket <- gs$best.tune$minbucket
-    cp <- gs$best.tune$cp
-    prune.cp <- gs$best.tune$prune.cp
-  } else {
-    gs <- NULL
-  }
-  parameters <- list(prune.cp = prune.cp,
-                     method = method,
-                     model = model,
-                     minsplit = minsplit,
-                     minbucket = minbucket,
-                     cp = cp,
-                     maxcompete = maxcompete,
-                     maxsurrogate = maxsurrogate,
-                     usesurrogate = usesurrogate,
-                     surrogatestyle = surrogatestyle,
-                     maxdepth = maxdepth,
-                     xval = xval,
-                     cost = cost,
-                     na.action = na.action)
+    # Arguments ====
+    if (is.null(y) & NCOL(x) < 2) {
+        print(args(s_CART))
+        stop("y is missing")
+    }
+    if (is.null(x.name)) x.name <- getName(x, "x")
+    if (is.null(y.name)) y.name <- getName(y, "y")
+    if (!verbose) print.plot <- FALSE
+    verbose <- verbose | !is.null(logFile)
+    if (save.mod & is.null(outdir)) outdir <- paste0("./s.", mod.name, "/")
+    if (!is.null(outdir)) outdir <- paste0(normalizePath(outdir, mustWork = FALSE), "/")
+    control <- list(
+        minsplit = minsplit,
+        minbucket = minbucket,
+        cp = cp,
+        maxcompete = maxcompete,
+        maxsurrogate = maxsurrogate,
+        usesurrogate = usesurrogate,
+        surrogatestyle = surrogatestyle,
+        maxdepth = maxdepth,
+        xval = xval
+    )
+    grid.search.type <- match.arg(grid.search.type)
 
-  # rpart ====
-  if (verbose) msg("Training CART...", newline.pre = TRUE)
-  mod <- rpart::rpart(formula = .formula,
-                      data = df.train,
-                      weights = .weights,
-                      method = method,
-                      model = model,
-                      control = control,
-                      cost = cost,
-                      parms = parms,
-                      na.action = na.action)
-
-  # Cost-Complexity Pruning ====
-  if (!is.null(prune.cp)) {
-    if (return.unpruned) mod.unpruned <- mod
-    if (use.prune.rpart.rt) {
-      mod <- prune.rpart.rt(mod, cp = prune.cp)
+    # Data ====
+    dt <- dataPrepare(x, y, x.test, y.test,
+        ipw = ipw,
+        ipw.type = ipw.type,
+        upsample = upsample,
+        downsample = downsample,
+        resample.seed = resample.seed,
+        verbose = verbose
+    )
+    x <- dt$x
+    y <- dt$y
+    x.test <- dt$x.test
+    y.test <- dt$y.test
+    xnames <- dt$xnames
+    type <- dt$type
+    .weights <- if (is.null(weights) & ipw) dt$weights else weights
+    x0 <- if (upsample | downsample) dt$x0 else x # x0, y0 are passed to gridSearchLearn
+    y0 <- if (upsample | downsample) dt$y0 else y
+    if (verbose) dataSummary(x, y, x.test, y.test, type)
+    if (type != "Survival") df.train <- data.frame(y = y, x)
+    if (method == "auto") {
+        if (type == "Regression") {
+            method <- "anova"
+            if (is.null(metric)) metric <- "MSE"
+            if (is.null(maximize)) maximize <- FALSE
+        } else if (type == "Classification") {
+            method <- "class"
+            if (is.null(metric)) metric <- "Balanced Accuracy"
+            if (is.null(maximize)) maximize <- TRUE
+        } else if (type == "Survival") {
+            method <- "exp"
+            if (is.null(metric)) metric <- "Concordance"
+            if (is.null(maximize)) maximize <- TRUE
+        }
+    }
+    if (is.null(cost)) cost <- rep(1, NCOL(x))
+    if (print.plot) {
+        if (is.null(plot.fitted)) plot.fitted <- if (is.null(y.test)) TRUE else FALSE
+        if (is.null(plot.predicted)) plot.predicted <- if (!is.null(y.test)) TRUE else FALSE
     } else {
-      mod <- rpart::prune(mod, cp = prune.cp)
+        plot.fitted <- plot.predicted <- FALSE
     }
-  }
+    if (is.null(metric)) {
+        if (type == "Classification") {
+            metric <- "Balanced Accuracy"
+        } else if (type == "Regression") {
+            metric <- "MSE"
+        } else {
+            metric <- "Concordance"
+        }
+    }
+    if (type == "Classification") n.classes <- length(levels(y))
 
-  # Fitted ====
-  fitted.prob <- NULL
-  if (type == "Regression" | type == "Survival") {
-    fitted <- predict(mod, x, type = "vector")
-  } else if (type == "Classification") {
-    if (n.classes == 2) {
-      fitted.prob <- predict(mod, x, type = "prob")[, 1]
+    # Formula ====
+    features <- paste(xnames, collapse = " + ")
+    .formula <- as.formula(paste0(y.name, " ~ ", features))
+
+    # Grid Search for prune.cp ====
+    if (gridCheck(maxdepth, minsplit, minbucket, cp, prune.cp)) {
+        gs <- gridSearchLearn(x0, y0,
+            mod = mod.name,
+            resample.rtset = grid.resample.rtset,
+            grid.params = list(
+                maxdepth = maxdepth,
+                minsplit = minsplit,
+                minbucket = minbucket,
+                cp = cp,
+                prune.cp = prune.cp
+            ),
+            fixed.params = list(
+                method = method,
+                model = model,
+                maxcompete = maxcompete,
+                maxsurrogate = maxsurrogate,
+                usesurrogate = usesurrogate,
+                surrogatestyle = surrogatestyle,
+                xval = xval,
+                cost = cost,
+                na.action = na.action,
+                ipw = ipw,
+                ipw.type = ipw.type,
+                upsample = upsample,
+                downsample = downsample,
+                resample.seed = resample.seed
+            ),
+            search.type = grid.search.type,
+            randomized.p = grid.randomized.p,
+            weights = weights,
+            metric = metric,
+            maximize = maximize,
+            verbose = grid.verbose,
+            n.cores = n.cores
+        )
+        maxdepth <- gs$best.tune$maxdepth
+        minsplit <- gs$best.tune$minsplit
+        minbucket <- gs$best.tune$minbucket
+        cp <- gs$best.tune$cp
+        prune.cp <- gs$best.tune$prune.cp
     } else {
-      fitted.prob <- predict(mod, x, type = "prob")
+        gs <- NULL
     }
-    fitted <- predict(mod, x, type = "class")
-  }
+    parameters <- list(
+        prune.cp = prune.cp,
+        method = method,
+        model = model,
+        minsplit = minsplit,
+        minbucket = minbucket,
+        cp = cp,
+        maxcompete = maxcompete,
+        maxsurrogate = maxsurrogate,
+        usesurrogate = usesurrogate,
+        surrogatestyle = surrogatestyle,
+        maxdepth = maxdepth,
+        xval = xval,
+        cost = cost,
+        na.action = na.action
+    )
 
+    # rpart ====
+    if (verbose) msg("Training CART...", newline.pre = TRUE)
+    mod <- rpart::rpart(
+        formula = .formula,
+        data = df.train,
+        weights = .weights,
+        method = method,
+        model = model,
+        control = control,
+        cost = cost,
+        parms = parms,
+        na.action = na.action
+    )
 
-  attr(fitted, "names") <- NULL
-  error.train <- modError(y, fitted, fitted.prob)
-  if (verbose) errorSummary(error.train, mod.name)
+    # Cost-Complexity Pruning ====
+    if (!is.null(prune.cp)) {
+        if (return.unpruned) mod.unpruned <- mod
+        if (use.prune.rpart.rt) {
+            mod <- prune.rpart.rt(mod, cp = prune.cp)
+        } else {
+            mod <- rpart::prune(mod, cp = prune.cp)
+        }
+    }
 
-  # Predicted ====
-  predicted.prob <- predicted <- error.test <- NULL
-  if (!is.null(x.test)) {
+    # Fitted ====
+    fitted.prob <- NULL
     if (type == "Regression" | type == "Survival") {
-      predicted <- predict(mod, x.test, type = "vector")
-      predicted.prob <- NULL
+        fitted <- predict(mod, x, type = "vector")
     } else if (type == "Classification") {
-      predicted.prob <- predict(mod, x.test, type = "prob")[, 1]
-      predicted <- predict(mod, x.test, type = "class")
+        if (n.classes == 2) {
+            fitted.prob <- predict(mod, x, type = "prob")[, 1]
+        } else {
+            fitted.prob <- predict(mod, x, type = "prob")
+        }
+        fitted <- predict(mod, x, type = "class")
     }
 
-    attr(predicted, "names") <- NULL
-    if (!is.null(y.test)) {
-      error.test <- modError(y.test, predicted, predicted.prob)
-      if (verbose) errorSummary(error.test, mod.name)
+
+    attr(fitted, "names") <- NULL
+    error.train <- modError(y, fitted, fitted.prob)
+    if (verbose) errorSummary(error.train, mod.name)
+
+    # Predicted ====
+    predicted.prob <- predicted <- error.test <- NULL
+    if (!is.null(x.test)) {
+        if (type == "Regression" | type == "Survival") {
+            predicted <- predict(mod, x.test, type = "vector")
+            predicted.prob <- NULL
+        } else if (type == "Classification") {
+            predicted.prob <- predict(mod, x.test, type = "prob")[, 1]
+            predicted <- predict(mod, x.test, type = "class")
+        }
+
+        attr(predicted, "names") <- NULL
+        if (!is.null(y.test)) {
+            error.test <- modError(y.test, predicted, predicted.prob)
+            if (verbose) errorSummary(error.test, mod.name)
+        } else {
+            error.test <- NULL
+        }
     } else {
-      error.test <- NULL
+        predicted <- predicted.prob <- error.test <- NULL
     }
-  } else {
-    predicted <- predicted.prob <- error.test <- NULL
-  }
 
-  # Outro ====
-  varimp <- rep(NA, NCOL(x))
-  varimp.cart <- if (!is.null(mod$variable.importance)) as.matrix(mod$variable.importance) else NULL
-  varimp.index <- match(rownames(varimp.cart), colnames(x))
-  varimp[varimp.index] <- varimp.cart
-  varimp[is.na(varimp)] <- 0
-  names(varimp) <- colnames(x)
-  extra <- list(imetrics = list(n.nodes = NROW(mod$frame),
-                                depth = max(tree.depth(as.numeric(rownames(mod$frame))))))
-  if (!is.null(prune.cp) & return.unpruned) extra$mod.unpruned <- mod.unpruned
-  rt <- rtModSet(rtclass = "rtMod",
-                 mod = mod,
-                 mod.name = mod.name,
-                 type = type,
-                 gridsearch = gs,
-                 parameters = parameters,
-                 call = call,
-                 y.train = y,
-                 y.test = y.test,
-                 x.name = x.name,
-                 y.name = y.name,
-                 xnames = xnames,
-                 fitted = fitted,
-                 fitted.prob = fitted.prob,
-                 se.fit = NULL,
-                 error.train = error.train,
-                 predicted = predicted,
-                 predicted.prob = predicted.prob,
-                 se.prediction = NULL,
-                 error.test = error.test,
-                 varimp = varimp,
-                 question = question,
-                 extra = extra)
+    # Outro ====
+    varimp <- rep(NA, NCOL(x))
+    varimp.cart <- if (!is.null(mod$variable.importance)) as.matrix(mod$variable.importance) else NULL
+    varimp.index <- match(rownames(varimp.cart), colnames(x))
+    varimp[varimp.index] <- varimp.cart
+    varimp[is.na(varimp)] <- 0
+    names(varimp) <- colnames(x)
+    extra <- list(imetrics = list(
+        n.nodes = NROW(mod$frame),
+        depth = max(tree.depth(as.numeric(rownames(mod$frame))))
+    ))
+    if (!is.null(prune.cp) & return.unpruned) extra$mod.unpruned <- mod.unpruned
+    rt <- rtModSet(
+        rtclass = "rtMod",
+        mod = mod,
+        mod.name = mod.name,
+        type = type,
+        gridsearch = gs,
+        parameters = parameters,
+        call = call,
+        y.train = y,
+        y.test = y.test,
+        x.name = x.name,
+        y.name = y.name,
+        xnames = xnames,
+        fitted = fitted,
+        fitted.prob = fitted.prob,
+        se.fit = NULL,
+        error.train = error.train,
+        predicted = predicted,
+        predicted.prob = predicted.prob,
+        se.prediction = NULL,
+        error.test = error.test,
+        varimp = varimp,
+        question = question,
+        extra = extra
+    )
 
-  rtMod.out(rt,
-            print.plot,
-            plot.fitted,
-            plot.predicted,
-            y.test,
-            mod.name,
-            outdir,
-            save.mod,
-            verbose,
-            plot.theme)
+    rtMod.out(
+        rt,
+        print.plot,
+        plot.fitted,
+        plot.predicted,
+        y.test,
+        mod.name,
+        outdir,
+        save.mod,
+        verbose,
+        plot.theme
+    )
 
-  outro(start.time, verbose = verbose, sinkOff = ifelse(is.null(logFile), FALSE, TRUE))
-  rt
-
+    outro(start.time, verbose = verbose, sinkOff = ifelse(is.null(logFile), FALSE, TRUE))
+    rt
 } # rtemis::s_CART
