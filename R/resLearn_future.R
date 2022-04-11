ea561051c0d043bad5f82ae1eae01c437426768a
Stathis <gennatas@gmail.com>
2022-04-11 01:26:51 -0700

    switching to future and progressr

---
+++ R/resLearn_future.R
@@ -0,0 +1,188 @@
+# resLearn_future.R
+# ::rtemis::
+# 2022 E.D. Gennatas lambdamd.org
+# new version of resLearn using future
+
+#' \pkg{rtemis} internal: Resample Learn
+#'
+#' Train an \pkg{rtemis} learner on a set of resamples
+#'
+#' Input: features (x) and outcome (y)
+#' Procedure: \link{resample}, train learners
+#' Output: trained learners
+#' This is used internally by \link{elevate} and for bagging, when the \code{bag.resampler}
+#' argument is set in a learner.
+#' @param x features - training set
+#' @param y outcome - training set
+#' @param mod Character: \pkg{rtemis} model. See \code{modSelect} gives available models
+#' @param resample.rtset List: output of \link{rtset} (or a list of same structure)
+#' @param params List of named elements, each is a single value
+#' @param verbose Logical: If TRUE, print messages to screen
+#' @param res.verbose Logical: Will be passed to each \code{mod}'s \code{verbose} argument
+#' @param save.mods Logical: If TRUE, save all models, otherwise discard after training.
+#' Use with \link{elevate} when training a large number of resamples. Default = TRUE
+#' @param outdir Character: Path to save output. Default = NULL
+#' @param n.cores Integer: Number of cores to use.
+#'
+#' @author E.D. Gennatas
+#' @export
+
+resLearn_future <- function(x, y, mod,
+                            resample.rtset = rtset.cv.resample(),
+                            weights = NULL,
+                            params = list(),
+                            mtry = NULL,
+                            .preprocess = NULL,
+                            verbose = TRUE,
+                            res.verbose = FALSE,
+                            trace = 0,
+                            save.mods = TRUE,
+                            outdir = NULL,
+                            n.cores = rtCores,
+                            parallel.type = "nobodycares") {
+
+    # Intro ----
+    start.time <- intro(
+        verbose = trace > 0,
+        message = "Starting resLearn...",
+        newline.pre = TRUE
+    )
+    rtemis_init(n.cores, context = "Outer resampling")
+
+    # Arguments ----
+    n.cores <- as.numeric(n.cores)[1]
+    if (missing(x) | missing(y)) {
+        print(args(resLearn))
+        stop("Input missing")
+    }
+    mod.name <- toupper(mod)
+    if (!is.null(outdir)) {
+        outdir <- normalizePath(outdir, mustWork = FALSE)
+        if (!dir.exists(outdir)) {
+            dir.create(outdir,
+                showWarnings = TRUE,
+                recursive = TRUE, mode = "0777"
+            )
+        }
+    }
+
+    # Resamples ----
+    learner <- modSelect(mod)
+    res <- resample(y, rtset = resample.rtset, verbose = trace > 0)
+    resampler <- attr(res, "type") # for res.group and res.index
+
+    if (n.cores > resample.rtset$n.resamples) n.cores <- resample.rtset$n.resamples
+    if (inherits(future::plan(), "sequential") && n.cores > 1) {
+        warning(
+            "n.cores set to ", n.cores, ", but future plan is sequential.",
+            "\n n.cores will be set to 1.",
+            "\nUse `plan(multisession)` or similar to allow parallel execution."
+        )
+        n.cores <- 1
+    }
+    if (n.cores > 1) future::plan(rtStrategy, workers = n.cores)
+    # if (verbose) msg("Running on", n.cores, singorplu(n.cores, "worker"))
+
+    # learner1 ----
+    p <- progressr::progressor(along = res)
+    learner1 <- function(index, learner,
+                         x, y,
+                         weights = NULL,
+                         mtry,
+                         res,
+                         params,
+                         .preprocess,
+                         verbose,
+                         outdir,
+                         save.mods,
+                         nres) {
+        if (verbose) msg("Running resample #", index, sep = "")
+        res1 <- res[[index]]
+        if (is.null(mtry)) {
+            feat.index <- seq(NCOL(x))
+        } else {
+            feat.index <- sample(seq(NCOL), mtry, replace = FALSE)
+        }
+        x.train1 <- x[res1, feat.index, drop = FALSE]
+        y.train1 <- y[res1]
+        x.test1 <- x[-res1, feat.index, drop = FALSE]
+        y.test1 <- y[-res1]
+
+        if (!is.null(.preprocess)) {
+            # This allows imputing training and testing sets separately
+            preproc.params <- c(list(x = x.train1), .preprocess, verbose = verbose)
+            x.train1 <- do.call(preprocess, preproc.params)
+            preproc.params <- c(list(x = x.test1), .preprocess, verbose = verbose)
+            x.test1 <- do.call(preprocess, preproc.params)
+        }
+
+        if (!is.null(weights)) weights <- weights[res1]
+
+        if (!is.null(outdir)) {
+            outdir1 <- paste0(outdir, "/", mod.name, ".resLearn.", index)
+        } else {
+            outdir1 <- NULL
+        }
+
+        if (!is.null(weights)) params$weights <- weights
+
+        args <- list(
+            x = x.train1, y = y.train1,
+            x.test = x.test1, y.test = y.test1,
+            print.plot = FALSE,
+            verbose = res.verbose,
+            outdir = outdir1
+        )
+        args <- c(args, params)
+
+        mod1 <- do.call(learner, args = args)
+        if (!save.mods) mod1$mod <- NA
+        p(sprintf("Outer resample: %i/%i...", index, nres))
+        list(
+            mod1 = mod1,
+            res.id = index,
+            params = params
+        )
+    }
+
+    # Res run ----
+    desc <- switch(resampler,
+        kfold = "independent folds",
+        strat.sub = "stratified subsamples",
+        strat.boot = "stratified bootstraps",
+        bootstrap = "bootstrap resamples",
+        loocv = "independent folds (LOOCV)",
+        "custom resamples"
+    )
+
+    if (verbose) {
+        msg0("Training ", modSelect(mod, desc = TRUE), " on ",
+            length(res), " ", desc, "...",
+            newline.pre = TRUE
+        )
+    }
+
+    res.run <- future.apply::future_lapply(
+        seq_along(res), learner1,
+        learner,
+        x, y,
+        weights,
+        mtry,
+        res,
+        params,
+        .preprocess,
+        verbose = res.verbose,
+        outdir = outdir,
+        save.mods = save.mods,
+        future.seed = TRUE,
+        length(res)
+    )
+    
+    names(res.run) <- paste0(toupper(mod), seq(res))
+    if (res.verbose) cat("\n")
+
+    # Outro ----
+    outro(start.time, verbose = trace > 0)
+    list(res = res, mods = res.run)
+
+} # rtemis::resLearn_future
--- R/rtemisInit.R
+++ R/rtemisInit.R
@@ -9,7 +9,7 @@
 #' Starts function execution timer and opens log file.
 #' Pairs with \code{outro}. Unfortunately the errors and warnings (the stderr; sink(type = "message"))
 #' cannot get split to file and console, so we keep in console only
-#' @noRd
+#' @keywords internal
 intro <- function(message = NULL,
                   verbose = TRUE,
                   as.message = FALSE,
@@ -17,34 +17,34 @@ intro <- function(message = NULL,
                   logFile = NULL,
                   call.depth = 1,
                   newline.pre = FALSE) {
-
-  if (!is.null(logFile)) {
-    logFile <- normalizePath(logFile, mustWork = FALSE)
-    outdir <- dirname(logFile)
-    if (!dir.exists(outdir)) dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
-    if (file.exists(logFile)) logFile <- paste0(logFile, "_1")
-    sink(logFile, split = verbose)
-  }
-  start.time <- proc.time()
-  if (verbose | !is.null(logFile)) {
-    if (newline.pre) cat("\n")
-    if (is.null(message)) {
-      msg("Hello,", Sys.getenv("USER"),
-          as.message = as.message,
-          call.depth = call.depth,
-          caller.id = 2,
-          color = color)
-    } else {
-      msg(message,
-          as.message = as.message,
-          call.depth = call.depth,
-          sep = "",
-          caller.id = 2,
-          color = color)
+    if (!is.null(logFile)) {
+        logFile <- normalizePath(logFile, mustWork = FALSE)
+        outdir <- dirname(logFile)
+        if (!dir.exists(outdir)) dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
+        if (file.exists(logFile)) logFile <- paste0(logFile, "_1")
+        sink(logFile, split = verbose)
     }
-  }
-  invisible(start.time)
-
+    start.time <- proc.time()
+    if (verbose | !is.null(logFile)) {
+        if (newline.pre) cat("\n")
+        if (is.null(message)) {
+            msg("Hello,", Sys.getenv("USER"),
+                as.message = as.message,
+                call.depth = call.depth,
+                caller.id = 2,
+                color = color
+            )
+        } else {
+            msg(message,
+                as.message = as.message,
+                call.depth = call.depth,
+                sep = "",
+                caller.id = 2,
+                color = color
+            )
+        }
+    }
+    invisible(start.time)
 } # rtemis::intro
 
 
@@ -55,7 +55,7 @@ intro <- function(message = NULL,
 #' Stops function execution timer and closes log file.
 #'
 #' Second part to \code{intro}
-#' @noRd
+#' @keywords internal
 outro <- function(start.time,
                   message = NULL,
                   verbose = TRUE,
@@ -64,57 +64,57 @@ outro <- function(start.time,
                   color = silver,
                   newline.pre = FALSE) {
 
-  # sinkOff must be FALSE by default since there is no logging by default, would result in warning
-  elapsed <- as.numeric(proc.time() - start.time)
-  if (verbose | sinkOff) {
-    if (newline.pre) cat("\n")
-    msg("Run completed in ", ddSci(elapsed[3] / 60), " minutes (",
-        "Real: ", ddSci(elapsed[3]), "; User: ", ddSci(elapsed[1]), "; System: ", ddSci(elapsed[2]), ")",
-        sep = "", as.message = as.message, caller.id = 2, color = color)
-  }
+    # sinkOff must be FALSE by default since there is no logging by default, would result in warning
+    elapsed <- as.numeric(proc.time() - start.time)
+    if (verbose | sinkOff) {
+        if (newline.pre) cat("\n")
+        msg("Run completed in ", ddSci(elapsed[3] / 60), " minutes (",
+            "Real: ", ddSci(elapsed[3]), "; User: ", ddSci(elapsed[1]), "; System: ", ddSci(elapsed[2]), ")",
+            sep = "", as.message = as.message, caller.id = 2, color = color
+        )
+    }
 
-  if (sinkOff) {
-    sink()
-  }
-  invisible(elapsed)
-
+    if (sinkOff) {
+        sink()
+    }
+    invisible(elapsed)
 } # rtemis::outro
 
 
 #' \code{rtemis-internals}: \code{dataSummary}
 #'
 #' Print input data dimensions and test dimensions match
-#' @noRd
+#' @keywords internal
 dataSummary <- function(x, y,
                         x.test = NULL, y.test = NULL,
                         type = NULL, testSet = TRUE) {
-  # cat("------------------------------------------------------\n")
-  # if (!is.null(type)) {
-  #   cat(paste(type, "Input Summary\n"))
-  # } else {
-  #   cat("Input Summary\n")
-  # }
-  # cat("------------------------------------------------------\n")
-  if (!is.null(type)) {
-    boxcat(paste(type, "Input Summary"), pad = 0)
-  } else {
-    boxcat("Input Summary", pad = 0)
-  }
-  cat("   Training features:", bold(NROW(x), "x", NCOL(x), "\n"))
-  cat("    Training outcome:", bold(NROW(y), "x", NCOL(y), "\n"))
+    # cat("------------------------------------------------------\n")
+    # if (!is.null(type)) {
+    #   cat(paste(type, "Input Summary\n"))
+    # } else {
+    #   cat("Input Summary\n")
+    # }
+    # cat("------------------------------------------------------\n")
+    if (!is.null(type)) {
+        boxcat(paste(type, "Input Summary"), pad = 0)
+    } else {
+        boxcat("Input Summary", pad = 0)
+    }
+    cat("Training features:", bold(NROW(x), "x", NCOL(x), "\n"))
+    cat(" Training outcome:", bold(NROW(y), "x", NCOL(y), "\n"))
 
-  if (testSet) {
-    if (!is.null(x.test)) {
-      cat("    Testing features:", bold(NROW(x.test), "x", NCOL(x.test), "\n"))
-    } else {
-      cat("    Testing features: Not available\n")
+    if (testSet) {
+        if (!is.null(x.test)) {
+            cat(" Testing features:", bold(NROW(x.test), "x", NCOL(x.test), "\n"))
+        } else {
+            cat(" Testing features: Not available\n")
+        }
+        if (!is.null(y.test)) {
+            cat("  Testing outcome:", bold(NROW(y.test), "x", NCOL(y.test), "\n"))
+        } else {
+            cat("  Testing outcome: Not available\n")
+        }
     }
-    if (!is.null(y.test)) {
-      cat("     Testing outcome:", bold(NROW(y.test), "x", NCOL(y.test), "\n"))
-    } else {
-      cat("     Testing outcome: Not available\n")
-    }
-  }
 } # rtemis::dataSummary
 
 
@@ -131,97 +131,144 @@ dataSummary <- function(x, y,
 #' @param pad Integer: Pad for \link{printls}
 #' @param newline.pre Logical: If TRUE, start with a newline. Default = TRUE
 #' @param newline Logical: If TRUE, end with anew (empty) line. Default = FALSE
-#' @export
+#'
 #' @author E.D. Gennatas
+#' @keywords internal
 
 parameterSummary <- function(...,
                              title = "Parameters",
                              pad = 0,
                              newline.pre = FALSE,
                              newline = FALSE) {
+    if (newline.pre) cat("\n")
+    if (length(list(...)) > 0) {
+        x <- list(...)
+        xnames <- as.character(substitute(list(...)))[-1L]
+        names(x) <- xnames
+        for (i in seq(x)) {
+            if (is.list(x[[i]]) && length(x[[i]]) > 0) {
+                # x[[i]] <- as.list(x[[i]]) # because data.frame also gives is.list TRUE
+                if (is.null(names(x[[i]]))) {
+                    names(x[[i]]) <- paste0(
+                        xnames[i], ".",
+                        seq_len(length(x[[i]]))
+                    )
+                }
+            }
+            if (is.null(x[[i]])) x[[i]] <- "NULL"
+        }
 
-  if (newline.pre) cat("\n")
-  if (length(list(...)) > 0) {
-    x = list(...)
-    xnames <- as.character(substitute(list(...)))[-1L]
-    names(x) <- xnames
-    for (i in seq(x)) {
-      if (is.list(x[[i]]) && length(x[[i]]) > 0) {
-        # x[[i]] <- as.list(x[[i]]) # because data.frame also gives is.list TRUE
-        if (is.null(names(x[[i]]))) names(x[[i]]) <- paste0(xnames[i], ".", 1:length(x[[i]]))
-      }
-      if (is.null(x[[i]])) x[[i]] <- "NULL"
+        printls(x, pad = pad + 3, title = title, center.title = FALSE)
+
+        if (newline) cat("\n")
     }
-
-    printls(x, pad = pad + 3, title = title, center.title = FALSE)
-
-    if (newline) cat("\n")
-  }
-
 } # rtemis::parameterSummary
 
 
 #' \code{rtemis-internals}: \code{gridSummary}
 #'
 #' Pretty print list of parameters with more than one value, which will be used in a grid search
-#' @noRd
+#' @keywords internal
 gridSummary <- function(...) {
-
-  params <- list(...)
-  xnames <- as.character(substitute(list(...)))[-1L]
-  index <- sapply(params, function(x) length(x) > 1)
-  tuned <- params[index]
-  names(tuned) <- xnames[index]
-  if (length(tuned) > 0) {
-    boxcat("Parameters to be tuned")
-    printls(tuned)
-    cat("\n")
-  }
-
+    params <- list(...)
+    xnames <- as.character(substitute(list(...)))[-1L]
+    index <- sapply(params, function(x) length(x) > 1)
+    tuned <- params[index]
+    names(tuned) <- xnames[index]
+    if (length(tuned) > 0) {
+        boxcat("Parameters to be tuned")
+        printls(tuned)
+        cat("\n")
+    }
 } # rtemis::gridSummary
 
 
 #' \code{rtemis-internals}: \code{errorSummary}
 #'
 #' Print Fit and Validation modError
-#' @noRd
+#' @keywords internal
 errorSummary <- function(error, mod.name = NULL, pre = NULL) {
+    id <- deparse(substitute(error))
+    if (class(error)[[1]] == "regError") {
+        type <- "Regression"
+    } else if (class(error)[[1]] == "survError") {
+        type <- "Survival"
+    } else {
+        type <- "Classification"
+    }
+    # type <- if (class(error)[[1]] == "regError") "Regression" else "Classification"
 
-  id <- deparse(substitute(error))
-  if (class(error)[[1]] == "regError") {
-    type <- "Regression"
-  } else if (class(error)[[1]] == "survError") {
-    type <- "Survival"
-  } else {
-    type <- "Classification"
-  }
-  # type <- if (class(error)[[1]] == "regError") "Regression" else "Classification"
+    if (is.null(pre)) {
+        if (grepl("train", id)) {
+            pre <- "Training"
+        } else if (grepl("test", id)) {
+            pre <- "Testing"
+        } else {
+            pre <- "Error"
+        }
+    }
 
-if (is.null(pre)) {
-  if (grepl('train', id)) {
-    pre <- "Training"
-  } else if (grepl('test', id)) {
-    pre <- "Testing"
-  } else {
-    pre <- "Error"
-  }
-}
-
-  if (!is.null(mod.name)) {
-    boxcat(paste(toupper(mod.name), type, pre, "Summary"))
-  } else {
-    boxcat(paste(type, pre, "Summary"))
-  }
-  print(error)
-
+    if (!is.null(mod.name)) {
+        boxcat(paste(toupper(mod.name), type, pre, "Summary"))
+    } else {
+        boxcat(paste(type, pre, "Summary"))
+    }
+    print(error)
 } # rtemis::errorSummary
 
 
 checkType <- function(type, allowed.types, mod.name) {
-
-  if (!type %in% allowed.types) {
-    rtStop("You were attempting to perform", type, "but", mod.name, "only supports:\n",
-           paste(allowed.types, collapse = ", "))
-  }
-
+    if (!type %in% allowed.types) {
+        rtStop(
+            "You were attempting to perform", type, "but", mod.name, "only supports:\n",
+            paste(allowed.types, collapse = ", ")
+        )
+    }
 }
+
+#' Initialize parallel processing and progress reporting
+#'
+#' @keywords internal
+
+rtemis_init <- function(n.cores = 1,
+                        context = NULL,
+                        verbose = TRUE) {
+
+    if (!is.null(context)) context <- paste0(context, ":")
+    if (n.cores > 1) {
+        rtPlan <- getOption(
+            "future.plan",
+            ifelse(.Platform$OS.type == "unix", "multicore", "multisession")
+        )
+        future::plan(rtPlan)
+        # rtenv$plan_set <- 1
+        if (verbose) {
+            msg(context, "Future plan set to", crayon::bold(rtPlan),
+                color = crayon::magenta,
+                "with", crayon::bold(n.cores), "workers"
+            )
+        }
+    } else {
+        future::plan("sequential")
+        if (verbose) {
+            msg(context, "Future plan set to", 
+                crayon::bold("sequential"),
+                color = crayon::magenta
+            )
+        }
+    }
+
+    if (is.null(rtenv$handlers_set)) {
+        rtProgress <- getOption("rt.progress", "global")
+        if (rtProgress == "global") progressr::handlers(global = TRUE)
+        rtHandler <- getOption("rt.handler", "progress")
+        progressr::handlers(rtHandler)
+        rtenv$handlers_set <- 1
+        if (verbose) {
+            msg("Global progression handler enabled using",
+                crayon::bold(rtHandler),
+                color = crayon::magenta
+            )
+        }
+    }
+} # rtemis::rtemis_init
