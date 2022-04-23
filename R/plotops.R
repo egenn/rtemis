7b0b10723ae115ea9078e52dc9d0dc13f1b017a8
Stathis <gennatas@gmail.com>
2022-04-23 13:12:02 -0700

    consolidate

--- R/plotops.R
+++ R/plotops.R
@@ -75,3 +75,36 @@ getlim <- function(x, axs = c("r", "i"), axs.r.pct = .04) {
   }
 } # rtemis::getlim
 
+# basegrid.R
+# ::rtemis::
+# 2022 E.D. Gennatas lambdamd.org
+
+basegrid <- function(x = NULL,
+                     y = NULL,
+                     col = NULL,
+                     lty = NULL,
+                     lwd = NULL,
+                     theme = getOption("rt.theme")) {
+
+  # Theme ----
+  if (is.character(theme)) {
+    theme <- do.call(paste0("theme_", theme), list())
+  } else {
+    stopifnot(is.list(theme))
+  }
+
+  if (is.null(col)) {
+    col <- colorAdjust(theme$grid.col, theme$grid.alpha)
+  }
+
+  if (is.null(lty)) {
+    lty <- theme$grid.lty
+  }
+
+  if (is.null(lwd)) {
+    lwd <- theme$grid.lwd
+  }
+
+  if (!is.null(x)) abline(v = x, col = col, lty = lty, lwd = lwd)
+  if (!is.null(y)) abline(h = y, col = col, lty = lty, lwd = lwd)
+} # rtemis::basegrid
