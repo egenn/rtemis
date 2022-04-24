47bfb42cde6bc92645da94d76ac9a73d7df8b158
Stathis <gennatas@gmail.com>
2022-04-23 19:17:35 -0700

    cleanup

--- R/plotly_utils.R
+++ R/plotly_utils.R
@@ -2,46 +2,51 @@
 # ::rtemis::
 # 2021 E.D. Gennatas lambdamd.org
 
-# plotly_vline calls plotly_vline1 to create a list for one or more vertical 
+# plotly_vline calls plotly_vline1 to create a list for one or more vertical
 # lines, to be passed to plotly::layout's shapes argument
-plotly_vline1 <- function(x, 
+plotly_vline1 <- function(x,
                           color = "#F48024",
                           width = 1,
                           dash = "dot") {
-  list(
-    type = "line",
-    x0 = x, x1 = x,
-    y0 = 0, y1 = 1, yref = "paper",
-    line = list(color = color,
-                width = width,
-                dash = dash)
-  )
+    list(
+        type = "line",
+        x0 = x, x1 = x,
+        y0 = 0, y1 = 1, yref = "paper",
+        line = list(
+            color = color,
+            width = width,
+            dash = dash
+        )
+    )
 }
 
-plotly_vline <- function(x, 
+plotly_vline <- function(x,
                          color = "#F48024",
                          width = 1,
                          dash = "dot") {
-  color <- recycle(color, x)
-  width <- recycle(width, x)
-  dash <- recycle(dash, x)
-  mapply(plotly_vline1, x, color = color, width = width, dash = dash,
-         SIMPLIFY = FALSE)
-  
+    color <- recycle(color, x)
+    width <- recycle(width, x)
+    dash <- recycle(dash, x)
+    mapply(plotly_vline1, x,
+        color = color, width = width, dash = dash,
+        SIMPLIFY = FALSE
+    )
 }
 
-plotly_hline1 <- function(y, 
+plotly_hline1 <- function(y,
                           color = "#F48024",
                           width = 1,
                           dash = "dot") {
-  list(
-    type = "line",
-    x0 = 0, x1 = 1,
-    y0 = y, y1 = y, xref = "paper",
-    line = list(color = color,
-                width = width,
-                dash = dash)
-  )
+    list(
+        type = "line",
+        x0 = 0, x1 = 1,
+        y0 = y, y1 = y, xref = "paper",
+        line = list(
+            color = color,
+            width = width,
+            dash = dash
+        )
+    )
 }
 
 plotly_hline <- function(y,
