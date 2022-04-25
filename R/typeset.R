bac28fa7fae52f26d10d7e1d8d937c7369f6cacf
Stathis <gennatas@gmail.com>
2022-04-24 22:42:32 -0700

    docs

--- R/typeset.R
+++ R/typeset.R
@@ -4,16 +4,21 @@
 
 #' Set type of columns
 #'
-#' Given an index of columns, convert identified columns of data.frame to factor, ordered factor,
-#' or integer. A number of datasets are distributed with an accompanying index of this sort,
-#' especially to define which variables should be treated as categorical (here, factors) for
-#' predicting modeling. This functions aims to make data type conversions in those cases easier.
+#' Given an index of columns, convert identified columns of data.frame to 
+#' factor, ordered factor, or integer. A number of datasets are distributed 
+#' with an accompanying index of this sort, especially to define which 
+#' variables should be treated as categorical (here, factors) for predicting 
+#' modeling. This functions aims to make data type conversions in those cases 
+#' easier.
 #'
 #' @param x data frame: input whose columns' types you want to edit
-#' @param factor.index Integer, vector: Index of columns to be converted to factors using \code{factor(x)}
-#' @param orderedfactor.index Integer, vector: Index of columns to be converted to ordered factors
-#' using \code{factor(x, ordered = TRUE)}
-#' @param integer.index Integer, vector: Index of columns to be converted to integers using \code{as.integer}
+#' @param factor.index Integer, vector: Index of columns to be converted to 
+#' factors using \code{factor(x)}
+#' @param orderedfactor.index Integer, vector: Index of columns to be 
+#' converted to ordered factors using \code{factor(x, ordered = TRUE)}
+#' @param integer.index Integer, vector: Index of columns to be converted to 
+#' integers using \code{as.integer}
+#' 
 #' @author E.D. Gennatas
 #' @export
 
@@ -22,20 +27,21 @@ typeset <- function(x,
                     orderedfactor.index = NULL,
                     integer.index = NULL) {
 
-  # [ Factors ] ----
+  # Factors ----
   if (!is.null(factor.index)) {
     for (i in factor.index) x[, i] <- factor(x[, i])
   }
 
-  # [ Ordered factors ] ----
+  # Ordered factors ----
   if (!is.null(orderedfactor.index)) {
     for (i in orderedfactor.index) x[, i] <- factor(x[, i], ordered = TRUE)
   }
 
-  # [ Integers ] ----
+  # Integers ----
   if (!is.null(integer.index)) {
     for (i in integer.index) x[, i] <- as.integer(x[, i])
   }
 
   x
+  
 } # rtemis::typeset
