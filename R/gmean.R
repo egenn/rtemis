7110339e6e9198a821c31daa8fb6d1366c44b24d
Stathis <gennatas@gmail.com>
2022-06-20 18:18:14 -0400

    init gmean

---
+++ R/gmean.R
@@ -0,0 +1,23 @@
+# gmean.R
+# ::rtemis::
+# 2022 E.D. Gennatas lambdamd.org
+
+#' Geometric mean
+#'
+#' @param x Numeric vector
+#'
+#' @author E.D. Gennatas
+#' @export
+#' @examples 
+#' x <- c(1, 3, 5)
+#' mean(x)
+#' gmean(x)
+#' #same as, but a little faster than:
+#' exp(mean(log(x)))
+
+
+gmean <- function(x) {
+
+    prod(x)^(1 / length(x))
+    
+} # rtemis::gmean
