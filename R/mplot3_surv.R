9ac4dd362beaa9ac2771b2a73720edc1f5a37c3e
Stathis <gennatas@gmail.com>
2022-07-06 23:04:02 -0700

    cleanup

--- R/mplot3_surv.R
+++ R/mplot3_surv.R
@@ -54,7 +54,7 @@ mplot3_surv <- function(x,
   # if (class(x)[[1]] != "Surv") stop("At least first object must be of type Survival")
   for (i in seq(x)) {
     # if (class(x[[i]]) != "Surv") {
-    if (!inherits(x[[i]]), "Surv") {
+    if (!inherits(x[[i]], "Surv")) {
       # Adding 1's assuming time to event for all
       x[[i]] <- survival::Surv(x[[i]], rep(1, length(x[[i]])))
     }
