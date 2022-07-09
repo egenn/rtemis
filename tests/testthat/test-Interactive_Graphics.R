baff820e3b1a9b56c034b37573036f2eaa3d5f84
Stathis <gennatas@gmail.com>
2022-07-08 20:49:16 -0700

    cleanup

--- tests/testthat/test-Interactive_Graphics.R
+++ tests/testthat/test-Interactive_Graphics.R
@@ -7,14 +7,14 @@ test_that("dplot3_box succeeds", {
     expect_type(dplot3_box(iris[, 1:3]), "list")
 })
 
-# mplot3_bar ----
+# dplot3_bar ----
 test_that("dplot3_bar succeeds", {
     x1 <- rnorm(10)
     names(x1) <- paste0("c", seq(x1))
     expect_type(dplot3_bar(x1), "list")
 })
 
-# mplot3_x ----
+# dplot3_x ----
 test_that("dplot3_x histogram succeeds", {
     expect_type(dplot3_x(rnorm(10), "hist"), "list")
 })
