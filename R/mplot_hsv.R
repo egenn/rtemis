dea5ae6cf685cc4c928c65b796820a8ac56e5b7c
Stathis <gennatas@gmail.com>
2022-08-27 09:09:19 -0700

    args

--- R/mplot_hsv.R
+++ R/mplot_hsv.R
@@ -12,31 +12,46 @@
 #' @param alpha Float: Alpha. 
 #' @param pch Integer: pch plot parameter. Default = 15 (square)
 #' @param bg Color: Background color. Default = "black"
-#' @param axes Logical: for \code{type = "cartesian"}: If TRUE, draw axes.
-#' @param pty Character: for \code{type = "cartesian"}: "s", "r", par's pty
+#' @param axes Logical: for \code{type = "square"}: If TRUE, draw axes.
+#' @param pty Character: for \code{type = "square"}: "s", "r", par's pty
 #' argument. Default = "s" (square plot)
 #' @param cex Float: \code{par/plot}'s cex argument.
-#' @param mar Float, vector: for \code{type = "cartesian"}: \code{par}'s mar
+#' @param mar Float, vector: for \code{type = "square"}: \code{par}'s mar
 #' argument.
 #' @param lab.col Color: Color for axes and labels. Defaults to inverse of
 #' \code{bg}, i.e. white if bg is black
-#' @param type Character: "cartesian" for square plot, "polar" for radial plot.
+#' @param type Character: "square" for square plot, "radial" for radial plot.
+#' @param show.grid Logical: if TRUE, show grid then type is "radial"
+#' @param par.reset Logical: If TRUE, reset \code{par} before exit
 #'
 #' @author E.D. Gennatas
 #' @export
+#' 
+#' @examples
+#' \dontrun{
+#' mplot_hsv()
+#' }
 
-mplot_hsv <- function(h.steps = seq(0, 1, .0125),
-                      s.steps = h.steps,
+mplot_hsv <- function(h.steps = seq(0, 1, .025),
+                      s.steps = seq(0, 1, .05),
                       v = 1,
                       alpha = 1,
-                      pch = 15,
+                      pch = 16,
                       bg = "black",
                       axes = TRUE,
                       pty = "s",
-                      cex = 1,
+                      cex = 2,
                       mar = c(3, 3, 2, .5),
                       lab.col = NULL,
-                      type = c("polar", "cartesian")) {
+                      type = c("radial", "square"),
+                      # radial
+                      line.col = "gray50",
+                      show.grid = TRUE,
+                      show.radial.grid = FALSE,
+                      show.grid.labels = 1,
+                      cex.axis = 1,
+                      cex.lab = 1,
+                      par.reset = TRUE) {
     
     # Arguments ----
     type <- match.arg(type)
@@ -52,7 +67,7 @@ mplot_hsv <- function(h.steps = seq(0, 1, .0125),
     grd <- expand.grid(h.steps, s.steps)
     col <- hsv(h = grd[, 1], s = grd[, 2], v = v, alpha = alpha)
 
-    if (type == "cartesian") {
+    if (type == "square") {
         # '- Square ----
         par(bg = bg, mar = mar, pty = pty)
         plot(grd,
@@ -66,8 +81,12 @@ mplot_hsv <- function(h.steps = seq(0, 1, .0125),
         )
 
         if (axes) {
-            axis(1, col = lab.col, col.ticks = lab.col, col.axis = lab.col)
-            axis(2, col = lab.col, col.ticks = lab.col, col.axis = lab.col)
+            axis(1,
+                col = lab.col, col.ticks = lab.col, col.axis = lab.col
+            )
+            axis(2,
+                col = lab.col, col.ticks = lab.col, col.axis = lab.col
+            )
         }
         mtext("H", 1, col = lab.col, line = 2, font = 2)
         mtext("S", 2, col = lab.col, line = 2, font = 2)
@@ -78,22 +97,28 @@ mplot_hsv <- function(h.steps = seq(0, 1, .0125),
 
         # '- Radial ----
         dependency_check("plotrix")
-        par(bg = bg)
+        par(bg = bg, cex.axis = cex.axis, cex.lab = cex.lab)
         plotrix::radial.plot(
             drange(grd[, 2], 0, 360 * pi / 180),
             drange(grd[, 1], 0, 360 * pi / 180),
+            labels = ddSci(h.steps),
             start = pi / 2,
             clockwise = TRUE,
             rp.type = "s",
-            point.symbols = 18,
+            point.symbols = pch,
             point.col = col,
+            line.col = line.col,
             label.prop = 1,
-            show.grid = T,
-            show.grid.labels = 1,
-            show.radial.grid = F,
+            show.grid = show.grid,
+            show.grid.labels = show.grid.labels,
+            show.radial.grid = show.radial.grid,
             radial.labels = "",
-            boxed.radial = FALSE
+            boxed.radial = FALSE,
+            cex = cex,
         )
-        mtext(paste0("HSV color (V = ", v, ")"), font = 2, col = lab.col)
+        mtext(
+            paste0("HSV color (V = ", v, ")"),
+            font = 1, col = lab.col
+        )
     }
 } # rtemis::mplot_hsv
