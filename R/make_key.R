f8a92b157ca510b7c02e8bca8b8bc4b3d0de0dd2
Stathis <gennatas@gmail.com>
2022-09-11 14:37:20 -0700

    init make_key

---
+++ R/make_key.R
@@ -0,0 +1,34 @@
+# make_key.R
+# ::rtemis::
+# 2022 E.D. Gennatas www.lambdamd.org
+
+#' Make key from data.table id - description columns
+#' 
+#' @param x Input data.table
+#' @param code_name Character: Name of column name that holds codes
+#' @param description_name Character: Name of column that holds descriptions
+#' @param filename Character: Path to file to save CSV with key
+#' 
+#' @author E.D. Gennatas
+#' @export 
+
+make_key <- function(x,
+                     code_name,
+                     description_name,
+                     filename = NULL) {
+    
+    dependency_check("data.table")
+
+    .key <- unique(
+        x,
+        by = code_name
+    )[, .SD, .SDcols = c(code_name, description_name)] |>
+        setkeyv(code_name)
+
+    if (!is.null(filename)) {
+        fwrite(.key, filename)
+    }
+    
+    .key
+
+} # rtemis:: make_key
