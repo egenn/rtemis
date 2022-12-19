621afd5c926cd07241ff6e96f661765fc865bd2f
Stathis <gennatas@gmail.com>
2022-12-19 16:50:52 +0000

    move rtrandom to new package

--- R/rt_fn.R
+++ R/rt_fn.R
@@ -456,61 +456,6 @@ popvar <- function(x) {
 }
 
 
-#' Random \pkg{rtemis} art
-#'
-#' Draw random shapes and colors
-#'
-#' @param pch Point character to use
-#' @param col Colors to use
-#' @param text Text to print
-#' @param text.col Color for text
-#' @author E.D. Gennatas
-#' @export
-
-rtrandom <- function(pch = sample(15:18, 1),
-                     col = rtCol3,
-                     text = "rtemis",
-                     text.col = "gray50",
-                     text.as.legend = FALSE,
-                     legend.bg = NULL,
-                     legend.alpha = 1,
-                     random.bg = TRUE) {
-    x <- runif(100, 1, 50)
-    y <- runif(100, 1, 50)
-    alpha <- runif(100, .1, 1)
-    color.alpha <- sapply(seq_along(col), function(x) adjustcolor(col[[x]], alpha[[x]]))
-    bg <- col
-    alpha <- runif(length(bg), .1, 1)
-    bg.alpha <- sapply(seq_along(bg), function(x) adjustcolor(bg[[x]], alpha[[x]]))
-    cex <- runif(length(col), 1, 10)
-    par.orig <- par(no.readonly = TRUE)
-    on.exit(suppressWarnings(par(par.orig)))
-    par(mar = c(0, 0, 0, 0))
-    plot(x, y,
-        pch = pch,
-        col = color.alpha,
-        bg = bg.alpha,
-        cex = cex, ann = F, axes = F, lwd = 3
-    )
-
-    if (!text.as.legend) {
-        text(
-            x = sample(10:40, 1), y = sample(10:40, 1),
-            labels = text, cex = 3, col = text.col
-        )
-    } else {
-        legend(
-            x = sample(10:40, 1), y = sample(10:40, 1),
-            text,
-            text.col = sample(col, 1),
-            box.lty = 0,
-            bg = adjustcolor(legend.bg, legend.alpha),
-            cex = 2
-        )
-    }
-} # rtemis::rtrandom
-
-
 #' Filter order
 #'
 #' @author E.D. Gennatas
