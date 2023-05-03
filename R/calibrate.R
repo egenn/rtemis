54a6ae6fcb540f0f850be06edc83d6f550df8bff
Stathis <gennatas@gmail.com>
2023-05-03 04:05:43 -0700

    add glm option

--- R/calibrate.R
+++ R/calibrate.R
@@ -13,15 +13,15 @@
 #' @param verbose Logical: If TRUE, printe messages to the console
 #'
 #' @return mod: fitted GAM model. Use `mod$fitted.values` to get calibrated
-#' input probabilited; use `predict(mod, newdata = newdata, type = "response")` 
+#' input probabilited; use `predict(mod, newdata = newdata, type = "response")`
 #' to calibrate other estimated probabilities.
-#' 
+#'
 #' @author EDG
 #' @export
 #' @examples
 #' \dontrun{
 #' data(segment_logistic, package = "probably")
-#' 
+#'
 #' # Plot the calibration curve of the original predictions
 #' dplot3_calibration(
 #'     true_labels = segment_logistic$Class,
@@ -33,27 +33,38 @@
 #' # Plot the calibration curve of the calibrated predictions
 #' dplot3_calibration(
 #'     true_labels = segment_logistic$Class,
-#'     est_prob = calibrate(segment_logistic$Class, 
-#'                          segment_logistic$.pred_poor)$fitted.values,
+#'     est_prob = calibrate(
+#'         segment_logistic$Class,
+#'         segment_logistic$.pred_poor
+#'     )$fitted.values,
 #'     n_windows = 10,
 #'     pos_class_idi = 2
 #' )
 #' }
 calibrate <- function(true_labels, est_prob,
                       pos_class_idi = 1,
+                      mod = c("gam", "glm"),
                       k = 5,
                       verbose = TRUE) {
-
     stopifnot(pos_class_idi %in% c(1, 2))
+    mod <- match.arg(mod)
     if (pos_class_idi == 1) {
         true_labels <- factor(true_labels, levels = rev(levels(true_labels)))
     }
 
     # GAM ----
     if (verbose) {
-        msg2start(paste0("Fitting GAM (k=", k, ")..."))
+        if (mod == "gam") {
+            msg2start(paste0("Fitting GAM (k=", k, ")..."))
+        } else {
+            msg2start("Fitting GLM...")
+        }
     }
-    mod <- mgcv::gam(true_labels ~ s(est_prob, k = k), family = binomial())
+    if (mod == "glm") {
+        mod <- glm(true_labels ~ est_prob, family = binomial())
+    } else {
+        mod <- mgcv::gam(true_labels ~ s(est_prob, k = k), family = binomial())
+    }
     if (verbose) {
         msg2done()
     }
