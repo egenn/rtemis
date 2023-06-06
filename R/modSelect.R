c49c5bc7e66b4d7de8e919306f1fad297763f2d5
Stathis <gennatas@gmail.com>
2023-06-05 21:27:50 -0400

    init tunable

--- R/modSelect.R
+++ R/modSelect.R
@@ -1,6 +1,95 @@
 # modSelect.R
 # ::rtemis::
-# 2016 E.D. Gennatas www.lambdamd.org
+# 2016-23 E.D. Gennatas www.lambdamd.org
+
+# Name + CRS ----
+rtMods <- data.frame(rbind(
+    c("TREELINED", "Lined Tree Learner", FALSE, TRUE, FALSE),
+    c("RGB", "Representational Gradient Boosting", TRUE, TRUE, TRUE),
+    c("AADDT", "Asymmetric Additive Tree", TRUE, TRUE, FALSE),
+    c("CSL", "Conditional SuperLearner", TRUE, TRUE, FALSE),
+    c("HAL", "Highly Adaptive LASSO", TRUE, TRUE, TRUE),
+    c("LINAD", "Linear Additive Tree", TRUE, TRUE, FALSE),
+    c("LINOA", "Linear Optimized Additive Tree", TRUE, TRUE, FALSE),
+    c("LITEBOOSTTV", "Lite Boosted Learner", FALSE, TRUE, FALSE),
+    c("ADABOOST", "Adaptive Boosting", TRUE, FALSE, FALSE),
+    c("ADDTREE", "Additive Tree", TRUE, FALSE, FALSE),
+    c("ADDTBOOSTTV", "Boosting of Additive Trees TV", FALSE, TRUE, FALSE),
+    c("BAG", "Bagged Learner", TRUE, TRUE, FALSE),
+    c("BOOST", "Boosted Learner", TRUE, TRUE, FALSE),
+    c("BART", "Bayesian Additive Regression Trees", TRUE, TRUE, FALSE),
+    c("BayesGLM", "Bayesian Generalized Linear Model", TRUE, TRUE, FALSE),
+    c("BOOST", "Boosted rtemis Model", FALSE, TRUE, FALSE),
+    c("BRUTO", "BRUTO Additive Model", FALSE, TRUE, FALSE),
+    c("CART", "Classification and Regression Trees", TRUE, TRUE, TRUE),
+    c("CARTLITE", "CART Lite", FALSE, TRUE, FALSE),
+    c("CARTLITEBOOST", "Boosted CART Lite", FALSE, TRUE, FALSE),
+    c("CARTLITEBOOSTTV", "Boosted CART Lite TV", FALSE, TRUE, FALSE),
+    c("CTREE", "Conditional Inference Trees", TRUE, TRUE, FALSE),
+    c("C50", "C5.0 Decision Tree", TRUE, FALSE, FALSE),
+    c("DN", "deepnet Neural Network", TRUE, TRUE, FALSE),
+    c("ET", "Extra Trees", TRUE, TRUE, FALSE),
+    c("EVTREE", "Evolutionary Learning of Globally Optimal Trees", TRUE, TRUE, FALSE),
+    c("GAM", "Generalized Additive Model", TRUE, TRUE, FALSE),
+    c("GAMSEL", "Regularized Generalized Additive Model", TRUE, TRUE, FALSE),
+    c("GAMSELX", "GAMSEL Interaction Model", FALSE, TRUE, FALSE),
+    c("GAMSELX2", "GAMSELX TOO", FALSE, TRUE, FALSE),
+    c("GBM", "Gradient Boosting Machine", TRUE, TRUE, TRUE),
+    c("GBM3", "Gradient Boosting Machine", TRUE, TRUE, TRUE),
+    c("GLM", "Generalized Linear Model", TRUE, TRUE, FALSE),
+    c("GLMLITE", "Lite GLM", FALSE, TRUE, FALSE),
+    c("GLMLITEBOOST", "Boosted GLM Lite", FALSE, TRUE, FALSE),
+    c("GLMLITEBOOSTTV", "Boosted GLM Lite TV", FALSE, TRUE, FALSE),
+    c("GLMNET", "Elastic Net", TRUE, TRUE, TRUE),
+    c("GLMTree", "Generalized Linear Model Tree", TRUE, TRUE, FALSE),
+    c("GLS", "Generalized Least Squares", FALSE, TRUE, FALSE),
+    c("H2ODL", "H2O Deep Learning", TRUE, TRUE, FALSE),
+    c("H2OGBM", "H2O Gradient Boosting Machine", TRUE, TRUE, FALSE),
+    c("H2ORF", "H2O Random Forest", TRUE, TRUE, FALSE),
+    c("LIHADBOOST", "Boosting of Additive Trees", FALSE, TRUE, FALSE),
+    c("LIHAD", "Linear Hard Additive Tree", FALSE, TRUE, FALSE),
+    c("KNN", "k-Nearest Neighbor", TRUE, TRUE, FALSE),
+    c("LDA", "Linear Discriminant Analysis", TRUE, FALSE, FALSE),
+    c("LightGBM", "LightGBM", TRUE, TRUE, FALSE),
+    c("LightRF", "LightGBM Random Forest", TRUE, TRUE, FALSE),
+    c("LightRuleFit", "LightGBM RuleFit", TRUE, TRUE, FALSE),
+    c("LM", "Ordinary Least Squares Regression", FALSE, TRUE, FALSE),
+    c("LOESS", "Local Polynomial Regression", FALSE, TRUE, FALSE),
+    c("LOGISTIC", "Logistic Regression", TRUE, FALSE, FALSE),
+    c("MARS", "Multivariate Adaptive Regression Splines", FALSE, TRUE, FALSE),
+    # c("MLGBM", "Spark MLlib Gradient Boosting", TRUE, TRUE, FALSE),
+    # c("MLMLP", "Spark MLlib Multilayer Perceptron", TRUE, FALSE, FALSE),
+    c("MLRF", "Spark MLlib Random Forest", TRUE, TRUE, FALSE),
+    c("MULTINOM", "Multinomial Logistic Regression", TRUE, FALSE, FALSE),
+    c("MXN", "MXNET Neural Network", TRUE, TRUE, FALSE),
+    c("NBayes", "Naive Bayes", TRUE, FALSE, FALSE),
+    c("NLA", "Nonlinear Activation Unit Regression", FALSE, TRUE, FALSE),
+    c("NLS", "Nonlinear Least Squares", FALSE, TRUE, FALSE),
+    c("NW", "Nadaraya-Watson Kernel Regression", FALSE, TRUE, FALSE),
+    c("POLY", "Polynomial Regression", FALSE, TRUE, FALSE),
+    c("POLYMARS", "Multivariate Adaptive Polynomial Spline Regression", FALSE, TRUE, FALSE),
+    c("POWER", "Power function using NLS", FALSE, TRUE, FALSE),
+    c("PPR", "Projection Pursuit Regression", FALSE, TRUE, FALSE),
+    c("PPTREE", "Projection Pursuit Tree", TRUE, FALSE, FALSE),
+    c("QDA", "Quadratic Discriminant Analysis", TRUE, FALSE, FALSE),
+    c("QRNN", "Quantile Neural Network Regression", FALSE, TRUE, FALSE),
+    c("RANGER", "Random Forest (ranger)", TRUE, TRUE, FALSE),
+    c("RF", "Random Forest", TRUE, TRUE, FALSE),
+    # c("RRF", "Regularized Random Forest", TRUE, TRUE, FALSE),
+    c("RFSRC", "Random Forest SRC", TRUE, TRUE, TRUE),
+    c("RLM", "Robust Linear Model", FALSE, TRUE, FALSE),
+    c("RuleFit", "RuleFit Ensemble Model", TRUE, TRUE, FALSE),
+    c("SDA", "Sparse Linear Discriminant Analysis", TRUE, FALSE, FALSE),
+    c("SGD", "Stochastic Gradient Descent", FALSE, TRUE, FALSE),
+    c("SPLS", "Sparse Partial Least Squares", FALSE, TRUE, FALSE),
+    c("SVM", "Support Vector Machine", TRUE, TRUE, FALSE),
+    c("TFN", "TensorFlow Neural Network", TRUE, TRUE, FALSE),
+    c("TLS", "Total Least Squares", FALSE, TRUE, FALSE),
+    c("XGBoost", "XGBoost", TRUE, TRUE, FALSE),
+    c("XGBLIN", "XGBoost with Linear Models", FALSE, TRUE, FALSE),
+    c("XRF", "XGBoost Random Forest", TRUE, TRUE, FALSE)
+))
+colnames(rtMods) <- c("rtemis name", "Description", "Class", "Reg", "Surv")
 
 #' Select \pkg{rtemis} Learner
 #'
@@ -18,94 +107,6 @@
 modSelect <- function(mod,
                       fn = FALSE,
                       desc = FALSE) {
-    # Name + CRS ----
-    rtMods <- data.frame(rbind(
-        c("TREELINED", "Lined Tree Learner", FALSE, TRUE, FALSE),
-        c("RGB", "Representational Gradient Boosting", TRUE, TRUE, TRUE),
-        c("AADDT", "Asymmetric Additive Tree", TRUE, TRUE, FALSE),
-        c("CSL", "Conditional SuperLearner", TRUE, TRUE, FALSE),
-        c("HAL", "Highly Adaptive LASSO", TRUE, TRUE, TRUE),
-        c("LINAD", "Linear Additive Tree", TRUE, TRUE, FALSE),
-        c("LINOA", "Linear Optimized Additive Tree", TRUE, TRUE, FALSE),
-        c("LITEBOOSTTV", "Lite Boosted Learner", FALSE, TRUE, FALSE),
-        c("ADABOOST", "Adaptive Boosting", TRUE, FALSE, FALSE),
-        c("ADDTREE", "Additive Tree", TRUE, FALSE, FALSE),
-        c("ADDTBOOSTTV", "Boosting of Additive Trees TV", FALSE, TRUE, FALSE),
-        c("BAG", "Bagged Learner", TRUE, TRUE, FALSE),
-        c("BOOST", "Boosted Learner", TRUE, TRUE, FALSE),
-        c("BART", "Bayesian Additive Regression Trees", TRUE, TRUE, FALSE),
-        c("BAYESGLM", "Bayesian Generalized Linear Model", TRUE, TRUE, FALSE),
-        c("BOOST", "Boosted rtemis Model", FALSE, TRUE, FALSE),
-        c("BRUTO", "BRUTO Additive Model", FALSE, TRUE, FALSE),
-        c("CART", "Classification and Regression Trees", TRUE, TRUE, TRUE),
-        c("CARTLITE", "CART Lite", FALSE, TRUE, FALSE),
-        c("CARTLITEBOOST", "Boosted CART Lite", FALSE, TRUE, FALSE),
-        c("CARTLITEBOOSTTV", "Boosted CART Lite TV", FALSE, TRUE, FALSE),
-        c("CTREE", "Conditional Inference Trees", TRUE, TRUE, FALSE),
-        c("C50", "C5.0 Decision Tree", TRUE, FALSE, FALSE),
-        c("DN", "deepnet Neural Network", TRUE, TRUE, FALSE),
-        c("ET", "Extra Trees", TRUE, TRUE, FALSE),
-        c("EVTREE", "Evolutionary Learning of Globally Optimal Trees", TRUE, TRUE, FALSE),
-        c("GAM", "Generalized Additive Model", TRUE, TRUE, FALSE),
-        c("GAMSEL", "Regularized Generalized Additive Model", TRUE, TRUE, FALSE),
-        c("GAMSELX", "GAMSEL Interaction Model", FALSE, TRUE, FALSE),
-        c("GAMSELX2", "GAMSELX TOO", FALSE, TRUE, FALSE),
-        c("GBM", "Gradient Boosting Machine", TRUE, TRUE, TRUE),
-        c("GBM3", "Gradient Boosting Machine", TRUE, TRUE, TRUE),
-        c("GLM", "Generalized Linear Model", TRUE, TRUE, FALSE),
-        c("GLMLITE", "Lite GLM", FALSE, TRUE, FALSE),
-        c("GLMLITEBOOST", "Boosted GLM Lite", FALSE, TRUE, FALSE),
-        c("GLMLITEBOOSTTV", "Boosted GLM Lite TV", FALSE, TRUE, FALSE),
-        c("GLMNET", "Elastic Net", TRUE, TRUE, TRUE),
-        c("GLMTREE", "Generalized Linear Model Tree", TRUE, TRUE, FALSE),
-        c("GLS", "Generalized Least Squares", FALSE, TRUE, FALSE),
-        c("H2ODL", "H2O Deep Learning", TRUE, TRUE, FALSE),
-        c("H2OGBM", "H2O Gradient Boosting Machine", TRUE, TRUE, FALSE),
-        c("H2ORF", "H2O Random Forest", TRUE, TRUE, FALSE),
-        c("LIHADBOOST", "Boosting of Additive Trees", FALSE, TRUE, FALSE),
-        c("LIHAD", "Linear Hard Additive Tree", FALSE, TRUE, FALSE),
-        c("KNN", "k-Nearest Neighbor", TRUE, TRUE, FALSE),
-        c("LDA", "Linear Discriminant Analysis", TRUE, FALSE, FALSE),
-        c("LightGBM", "LightGBM", TRUE, TRUE, FALSE),
-        c("LightRF", "LightGBM Random Forest", TRUE, TRUE, FALSE),
-        c("LightRuleFit", "LightGBM RuleFit", TRUE, TRUE, FALSE),
-        c("LM", "Ordinary Least Squares Regression", FALSE, TRUE, FALSE),
-        c("LOESS", "Local Polynomial Regression", FALSE, TRUE, FALSE),
-        c("LOGISTIC", "Logistic Regression", TRUE, FALSE, FALSE),
-        c("MARS", "Multivariate Adaptive Regression Splines", FALSE, TRUE, FALSE),
-        # c("MLGBM", "Spark MLlib Gradient Boosting", TRUE, TRUE, FALSE),
-        # c("MLMLP", "Spark MLlib Multilayer Perceptron", TRUE, FALSE, FALSE),
-        c("MLRF", "Spark MLlib Random Forest", TRUE, TRUE, FALSE),
-        c("MULTINOM", "Multinomial Logistic Regression", TRUE, FALSE, FALSE),
-        c("MXN", "MXNET Neural Network", TRUE, TRUE, FALSE),
-        c("NBAYES", "Naive Bayes", TRUE, FALSE, FALSE),
-        c("NLA", "Nonlinear Activation Unit Regression", FALSE, TRUE, FALSE),
-        c("NLS", "Nonlinear Least Squares", FALSE, TRUE, FALSE),
-        c("NW", "Nadaraya-Watson Kernel Regression", FALSE, TRUE, FALSE),
-        c("POLY", "Polynomial Regression", FALSE, TRUE, FALSE),
-        c("POLYMARS", "Multivariate Adaptive Polynomial Spline Regression", FALSE, TRUE, FALSE),
-        c("POWER", "Power function using NLS", FALSE, TRUE, FALSE),
-        c("PPR", "Projection Pursuit Regression", FALSE, TRUE, FALSE),
-        c("PPTREE", "Projection Pursuit Tree", TRUE, FALSE, FALSE),
-        c("QDA", "Quadratic Discriminant Analysis", TRUE, FALSE, FALSE),
-        c("QRNN", "Quantile Neural Network Regression", FALSE, TRUE, FALSE),
-        c("RANGER", "Random Forest (ranger)", TRUE, TRUE, FALSE),
-        c("RF", "Random Forest", TRUE, TRUE, FALSE),
-        # c("RRF", "Regularized Random Forest", TRUE, TRUE, FALSE),
-        c("RFSRC", "Random Forest SRC", TRUE, TRUE, TRUE),
-        c("RLM", "Robust Linear Model", FALSE, TRUE, FALSE),
-        c("RuleFit", "RuleFit Ensemble Model", TRUE, TRUE, FALSE),
-        c("SDA", "Sparse Linear Discriminant Analysis", TRUE, FALSE, FALSE),
-        c("SGD", "Stochastic Gradient Descent", FALSE, TRUE, FALSE),
-        c("SPLS", "Sparse Partial Least Squares", FALSE, TRUE, FALSE),
-        c("SVM", "Support Vector Machine", TRUE, TRUE, FALSE),
-        c("TFN", "TensorFlow Neural Network", TRUE, TRUE, FALSE),
-        c("TLS", "Total Least Squares", FALSE, TRUE, FALSE),
-        c("XGBOOST", "XGBoost", TRUE, TRUE, FALSE),
-        c("XGBLIN", "XGBoost with Linear Models", FALSE, TRUE, FALSE),
-        c("XRF", "XGBoost Random Forest", TRUE, TRUE, FALSE)
-    ))
-    colnames(rtMods) <- c("rtemis name", "Description", "Class", "Reg", "Surv")
 
     # if (missing(mod) & !listAliases) {
     if (missing(mod)) {
@@ -125,7 +126,7 @@ modSelect <- function(mod,
     }
 
     name.vec <- toupper(rtMods[, 1])
-    name <- rtMods[, 1][pmatch(toupper(mod), name.vec)]
+    name <- grep(mod, rtMods[, 1], ignore.case = TRUE, value = TRUE)
     if (is.na(name)) {
         print(rtMods[, 1:2], quote = FALSE)
         stop(mod, ": Incorrect model specified")
@@ -150,9 +151,9 @@ modSelect <- function(mod,
 
 # wip
 gsc <- c(
-    "ADDTREE", "CART", "DN", "GBM", "GBM0", "GBM3", "GLMNET", "GLMTREE",
+    "ADDTREE", "CART", "DN", "GBM", "GBM0", "GBM3", "GLMNET", "GLMTree",
     "H2OGBM", "LIHAD", "LINAD", "LINOA", "MARS", "POLYMARS", "PPR", "RANGER",
-    "RF", "SPLS", "SVM", "XGBOOST", "LIGHTGBM", "LIGHTRF"
+    "RF", "SPLS", "SVM", "XGBoost", "LIGHTGBM", "LIGHTRF"
 )
 
 
@@ -166,48 +167,9 @@ RT_tunable <- data.table(
     )
 )
 
-tunable <- function(mod) {
-    msg(hilite(modSelect(mod, desc = TRUE)), "tunable hyperparameters:")
-    # printchar(
-    #     RT_tunable[Algorithm == toupper(mod), `Tunable Hyperparameters`][[1]] |> as.list()
-    # )
-    printls(mod_params[[toupper(mod)]])
-}
-
-gbm_params <- list(
-    interaction.depth = "Integer: Interaction depth.",
-    shrinkage = "Float: Shrinkage (learning rate).",
-    n.minobsinnode = "Integer: Minimum number of observation allowed in node.",
-    bag.fraction = "Float (0, 1): Fraction of cases to use to train each tree."
-)
-
-ranger_params <- list(
-    mtry = "Integer: Number of variables to randomly sample as candidates at each split."
-)
-
 glmnet_params <- list(
     alpha = "Float (0, 1): Elastic net mixing parameter.",
     lambda = "Float: Regularization parameter."
-)
-
-cart_params <- list(
-    cp = "Numeric: Complexity parameter.",
-    maxdepth = "Integer: Maximum depth of tree.",
-    minbucket = "Integer: Minimum number of observations in a terminal node.",
-    minsplit = "Integer: Minimum number of observations in a node to split.",
-    prune.cp = "Numeric: Complexity for cost-complexity pruning after tree is built"
-)
-
-xgboost_params <- list(
-    eta = "Float (0, 1): Learning rate.",
-    gamma = "Float: Minimum loss reduction required to make a further partition on a leaf node of the tree.",
-    max_depth = "Integer: Maximum depth of a tree.",
-    min_child_weight = "Integer: Minimum sum of instance weight (hessian) needed in a child.",
-    subsample = "Float (0, 1): Subsample ratio of the training instances.",
-    colsample_bytree = "Float (0, 1): Subsample ratio of columns when constructing each tree.",
-    colsample_bylevel = "Float (0, 1): Subsample ratio of columns for each split, in each level.",
-    lambda = "Float: L2 regularization term on weights.",
-    alpha = "Float: L1 regularization term on weights."
 )
 
 svm_params <- list(
@@ -225,7 +187,63 @@ svm_params <- list(
     max_iter = "Integer: Hard limit on iterations within solver, or -1 for no limit."
 )
 
-mod_params <- list(
-    GBM = gbm_params, CART = cart_params,
-    RANGER = ranger_params, GLMNET = glmnet_params
+cart_params <- list(
+    cp = "Numeric: Complexity parameter.",
+    maxdepth = "Integer: Maximum depth of tree.",
+    minbucket = "Integer: Minimum number of observations in a terminal node.",
+    minsplit = "Integer: Minimum number of observations in a node to split.",
+    prune.cp = "Numeric: Complexity for cost-complexity pruning after tree is built"
 )
+
+ranger_params <- list(
+    mtry = "Integer: Number of variables to randomly sample as candidates at each split."
+)
+
+gbm_params <- list(
+    interaction.depth = "Integer: Interaction depth.",
+    shrinkage = "Float: Shrinkage (learning rate).",
+    n.minobsinnode = "Integer: Minimum number of observation allowed in node.",
+    bag.fraction = "Float (0, 1): Fraction of cases to use to train each tree."
+)
+
+xgboost_params <- list(
+    eta = "Float (0, 1): Learning rate.",
+    gamma = "Float: Minimum loss reduction required to make a further partition on a leaf node of the tree.",
+    max_depth = "Integer: Maximum depth of a tree.",
+    min_child_weight = "Integer: Minimum sum of instance weight (hessian) needed in a child.",
+    subsample = "Float (0, 1): Subsample ratio of the training instances.",
+    colsample_bytree = "Float (0, 1): Subsample ratio of columns when constructing each tree.",
+    colsample_bylevel = "Float (0, 1): Subsample ratio of columns for each split, in each level.",
+    lambda = "Float: L2 regularization term on weights.",
+    alpha = "Float: L1 regularization term on weights."
+)
+
+lightgbm_params <- list(
+    num_leaves = "Integer: Maximum tree leaves for base learners.", 
+    max_depth = "Integer: Maximum tree depth for base learners, -1 means no limit.",
+    learning_rate = "Float: Boosting learning rate.", 
+    bagging_fraction = "Float: Subsample ratio of the training set.",
+    lambda_l1 = "Float: L1 regularization.", 
+    lambda_l2 = "Float: L2 regularization."
+)
+
+alg_params <- list(
+    GLMNET = glmnet_params,
+    SVM = svm_params,
+    CART = cart_params,
+    RANGER = ranger_params, 
+    GBM = gbm_params, 
+    XGBoost = xgboost_params,
+    LightGBM = lightgbm_params
+)
+
+#' Print tunable hyperparameters for a supervised learning algorithm
+#' 
+#' 
+tunable <- function(alg = c("glmnet", "svm", "cart", 
+                            "ranger", "gbm", "xgboost", 
+                            "lightgbm")) {
+    modname <- grep(alg, rtMods[, 1], ignore.case = TRUE, value = TRUE)
+    msg2(hilite(modSelect(alg, desc = TRUE)), "tunable hyperparameters:")
+    printls(alg_params[[modname]])
+}
