931e4d8dcc78ce9efb23c1781b21dbaddbc5efbf
Stathis <gennatas@gmail.com>
2023-10-09 02:10:15 -0700

    revert %notin% but do not export; data.table %notin% is, uhm, not in CRAN version currently.

---
+++ R/logical_ops.R
@@ -0,0 +1,19 @@
+# logical_ops
+# ::rtemis::
+# 2023 EDG lambdamd.org
+
+#' Inverse of `%in%`
+#'
+#' data.table %notin% not currently on CRAN version
+#' 
+#' @param x vector
+#' @param set vector
+#'
+#' @returns logical vector
+#' @keywords internal
+#' @noRd
+#'
+#' @examples
+#' 3 %notin% c(2, 4, 6) # TRUE
+#' 4 %notin% 3:5 # FALSE
+"%notin%" <- function(x, set) !x %in% set
