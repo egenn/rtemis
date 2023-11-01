56c1c2362b990240ab27c3ae6631a77a8e1e10e7
Stathis <gennatas@gmail.com>
2023-11-01 11:22:48 -0700

    cleanup

--- tests/testthat/factoryze_tests.R
+++ tests/testthat/factoryze_tests.R
@@ -7,20 +7,19 @@ options(rt.font = "sans")
 if (requireNamespace("psych", quietly = TRUE) &&
   requireNamespace("GPArotation", quietly = TRUE) &&
   requireNamespace("lavaan", quietly = TRUE)) {
+  # Library ----
+  library(rtemis)
 
-# Library ----
-library(rtemis)
+  # Data ----
+  set.seed(2019)
+  n.cases <- 500
+  n.factors <- 4
+  n.varsPerFactor <- 5
+  f <- lapply(seq(n.factors), function(i) rnorm(n.cases))
+  x <- do.call(cbind, lapply(seq(n.factors), function(i) {
+    sapply(seq(n.varsPerFactor), function(j) rnorm(1) * f[[i]] + rnorm(500))
+  }))
 
-# Data ----
-set.seed(2019)
-n.cases <- 500
-n.factors <- 4
-n.varsPerFactor <- 5
-f <- lapply(seq(n.factors), function(i) rnorm(n.cases))
-x <- do.call(cbind, lapply(seq(n.factors), function(i)
-  sapply(seq(n.varsPerFactor), function(j) rnorm(1) * f[[i]] + rnorm(500))))
-
-# factoryze ----
-x_fac <- factoryze(x)
-
+  # factoryze ----
+  x_fac <- factoryze(x)
 } # if (requireNamespace(...))
