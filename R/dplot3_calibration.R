71cfaa36eb72623ac036add3ed77f0dce98f49d9
Stathis <gennatas@gmail.com>
2024-01-07 00:38:06 +0200

    normalize arg names

--- R/dplot3_calibration.R
+++ R/dplot3_calibration.R
@@ -4,10 +4,10 @@
 
 #' Draw calibration plot
 #'
-#' @param true_labels Factor with true class labels
-#' @param est_prob Numeric vector with predicted probabilities
-#' @param n_windows Integer: Number of windows to split the data into
-#' @param pos_class_idi Integer: Index of the positive class
+#' @param true.labels Factor with true class labels
+#' @param est.prob Numeric vector with predicted probabilities
+#' @param n.windows Integer: Number of windows to split the data into
+#' @param pos.class.idi Integer: Index of the positive class
 #' @param xlab Character: x-axis label
 #' @param ylab Character: y-axis label
 #' @param mode Character: Plot mode
@@ -22,64 +22,64 @@
 #'
 #' # Plot the calibration curve of the original predictions
 #' dplot3_calibration(
-#'   true_labels = segment_logistic$Class,
-#'   est_prob = segment_logistic$.pred_poor,
-#'   n_windows = 10,
-#'   pos_class_idi = 2
+#'   true.labels = segment_logistic$Class,
+#'   est.prob = segment_logistic$.pred_poor,
+#'   n.windows = 10,
+#'   pos.class.idi = 2
 #' )
 #'
 #' # Plot the calibration curve of the calibrated predictions
 #' dplot3_calibration(
-#'   true_labels = segment_logistic$Class,
-#'   est_prob = calibrate(
+#'   true.labels = segment_logistic$Class,
+#'   est.prob = calibrate(
 #'     segment_logistic$Class,
 #'     segment_logistic$.pred_poor
 #'   )$fitted.values,
-#'   n_windows = 10,
-#'   pos_class_idi = 2
+#'   n.windows = 10,
+#'   pos.class.idi = 2
 #' )
 #' }
-dplot3_calibration <- function(true_labels, est_prob,
-                               n_windows = 10,
-                               pos_class_idi = 1,
+dplot3_calibration <- function(true.labels, est.prob,
+                               n.windows = 10,
+                               pos.class.idi = 1,
                                xlab = "Mean estimated probability",
                                ylab = "Empirical risk",
                                #    conf_level = .95,
                                mode = "markers+lines", ...) {
-  if (!is.list(est_prob)) {
-    est_prob <- list(estimated_prob = est_prob)
+  if (!is.list(est.prob)) {
+    est.prob <- list(estimated_prob = est.prob)
   }
-  pos_class <- levels(true_labels)[pos_class_idi]
+  pos_class <- levels(true.labels)[pos.class.idi]
 
   # Create windows
-  breaks <- seq(0, 1, length.out = n_windows + 1)
+  breaks <- seq(0, 1, length.out = n.windows + 1)
 
   # Calculate the mean probability in each window
-  # mean_window_prob <- sapply(seq_len(n_windows), \(i) {
-  #     mean(est_prob[est_prob >= breaks[i] & est_prob < breaks[i + 1]])
+  # mean_window_prob <- sapply(seq_len(n.windows), \(i) {
+  #     mean(est.prob[est.prob >= breaks[i] & est.prob < breaks[i + 1]])
   # })
-  mean_window_prob <- lapply(seq_along(est_prob), \(i) {
-    sapply(seq_len(n_windows), \(j) {
-      mean(est_prob[[i]][est_prob[[i]] >= breaks[j] & est_prob[[i]] < breaks[j + 1]])
+  mean_window_prob <- lapply(seq_along(est.prob), \(i) {
+    sapply(seq_len(n.windows), \(j) {
+      mean(est.prob[[i]][est.prob[[i]] >= breaks[j] & est.prob[[i]] < breaks[j + 1]])
     })
   })
 
   # Calculate the proportion of condition positive cases in each window
-  # window_empirical_risk <- sapply(seq_len(n_windows), \(i) {
-  #     idl <- est_prob >= breaks[i] & est_prob < breaks[i + 1]
-  #     sum(true_labels[idl] == pos_class) / sum(idl)
+  # window_empirical_risk <- sapply(seq_len(n.windows), \(i) {
+  #     idl <- est.prob >= breaks[i] & est.prob < breaks[i + 1]
+  #     sum(true.labels[idl] == pos_class) / sum(idl)
   # })
-  window_empirical_risk <- lapply(seq_along(est_prob), \(i) {
-    sapply(seq_len(n_windows), \(j) {
-      idl <- est_prob[[i]] >= breaks[j] & est_prob[[i]] < breaks[j + 1]
-      sum(true_labels[idl] == pos_class) / sum(idl)
+  window_empirical_risk <- lapply(seq_along(est.prob), \(i) {
+    sapply(seq_len(n.windows), \(j) {
+      idl <- est.prob[[i]] >= breaks[j] & est.prob[[i]] < breaks[j + 1]
+      sum(true.labels[idl] == pos_class) / sum(idl)
     })
   })
 
   # Calculate confidence intervals
-  # confint <- sapply(seq_len(n_windows), \(i) {
-  #     events <- length(true_labels[true_labels == pos_class & est_prob >= breaks[i] & est_prob < breaks[i + 1]])
-  #     total <- length(est_prob >= breaks[i] & est_prob < breaks[i + 1])
+  # confint <- sapply(seq_len(n.windows), \(i) {
+  #     events <- length(true.labels[true.labels == pos_class & est.prob >= breaks[i] & est.prob < breaks[i + 1]])
+  #     total <- length(est.prob >= breaks[i] & est.prob < breaks[i + 1])
   #     suppressWarnings(pt <- prop.test(
   #         events, total,
   #         conf.level = conf_level
