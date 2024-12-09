7ae5487b87543a5da77bb8f271cdac4c679743a8
Stathis Gennatas <gennatas@gmail.com>
2024-12-09 10:43:29 -0800

    support all learners

--- R/calibrate.R
+++ R/calibrate.R
@@ -2,18 +2,16 @@
 # ::rtemis::
 # 2023 EDG rtemis.org
 
-#' Calibrate predicted probabilities using GAM
+#' Calibrate predicted probabilities
 #'
-#' Calibrate predicted probabilities using a generalized additive model (GAM).
-#'
-#' @param true.labels Factor with true class labels
-#' @param predicted.prob Numeric vector with predicted probabilities
-#' @param pos.class Integer: Index of the positive class
-#' @param mod Character: Model to use for calibration. Either "gam" or "glm"
-#' @param k Integer: GAM degrees of freedom
+#' @param true.labels Factor with true class labels.
+#' @param predicted.prob Numeric vector with predicted probabilities.
+#' @param pos.class Integer: Index of the positive class.
+#' @param alg Character: Algorithm to use to train calibration model. See [select_learn()].
+#' @param learn.params List: List of parameters to pass to the learning algorithm
 #' @param verbose Logical: If TRUE, print messages to the console
 #'
-#' @return mod: fitted GAM model. Use `mod$fitted.values` to get calibrated
+#' @return Trained calibration model. Use `$fitted.values` to get calibrated
 #' input probabilities; use `predict(mod, newdata = newdata, type = "response")`
 #' to calibrate other estimated probabilities.
 #'
@@ -21,51 +19,46 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' data(segment_logistic, package = "probably")
+#' data("segment_naive_bayes", package = "probably")
 #'
 #' # Plot the calibration curve of the original predictions
 #' dplot3_calibration(
-#'   true.labels = segment_logistic$Class,
-#'   predicted.prob = segment_logistic$.pred_poor,
-#'   n_windows = 10,
+#'   true.labels = segment_naive_bayes$Class,
+#'   predicted.prob = segment_naive_bayes$.pred_poor,
 #'   pos.class = 2
 #' )
 #'
 #' # Plot the calibration curve of the calibrated predictions
 #' dplot3_calibration(
-#'   true.labels = segment_logistic$Class,
+#'   true.labels = segment_naive_bayes$Class,
 #'   predicted.prob = calibrate(
-#'     segment_logistic$Class,
-#'     segment_logistic$.pred_poor
+#'     segment_naive_bayes$Class,
+#'     segment_naive_bayes$.pred_poor
 #'   )$fitted.values,
-#'   n_windows = 10,
 #'   pos.class = 2
 #' )
 #' }
 calibrate <- function(true.labels,
                       predicted.prob,
                       pos.class = NULL,
-                      mod = c("gam", "glm"),
-                      k = 5,
+                      alg = "isotonic",
+                      learn.params = list(),
                       verbose = TRUE) {
-  stopifnot(pos.class %in% c(1, 2))
-  mod <- match.arg(mod)
+  # Check positive class
   if (is.null(pos.class)) {
     pos.class <- rtenv$binclasspos
   }
-  if (pos.class == 1) {
+  if (pos.class == 2) {
     true.labels <- factor(true.labels, levels = rev(levels(true.labels)))
   }
+  alg_name <- select_learn(alg, desc = TRUE)
 
-  # GLAM ----
-  if (mod == "glm") {
-    mod <- glm(true.labels ~ predicted.prob, family = binomial())
-  } else if (mod == "gam") {
-    mod <- mgcv::gam(true.labels ~ s(predicted.prob, k = k), family = binomial())
-  }
-  if (verbose) {
-    msg2done()
-  }
-  mod
+  # mod ----
+  if (verbose) msg20("Calibrating probabilities using ", alg_name, "...")
+  learner <- select_learn(alg)
+  do.call(
+    learner,
+    c(list(x = predicted.prob, y = true.labels), learn.params)
+  )
 
 } # rtemis::calibrate
