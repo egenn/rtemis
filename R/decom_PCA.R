5e435ec8abf5d3f44231bef6ccc63d5a793bb493
Stathis Gennatas <gennatas@gmail.com>
2025-02-02 06:17:19 -0800

    init

---
+++ R/decom_PCA.R
@@ -0,0 +1,23 @@
+# decom_PCA.R
+# ::rtemis::
+# 2025 EDG rtemis.org
+
+#' PCA Decomposition
+#' 
+decom_PCA <- function(x, parameters, verbosity = 1L) {
+  # Checks ----
+  check_is_S7(parameters, PCAParameters)
+  check_unsupervised_data(x = x, allow_missing = FALSE)
+
+  # Decompose ----
+  decom <- prcomp(
+    x = x,
+    center = parameters$center,
+    scale. = parameters$scale,
+    tol = parameters$tol,
+    rank. = parameters$k
+  )
+  check_inherits(decom, "prcomp")
+  list(decom = decom, projections = decom$x)
+
+} # /rtemis::decom_ICA
