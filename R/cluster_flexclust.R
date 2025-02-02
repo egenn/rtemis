9ba461864bf3121e4113c0e970b0d6ca52158746
Stathis Gennatas <gennatas@gmail.com>
2025-02-01 21:03:51 -0800

    init

---
+++ R/cluster_flexclust.R
@@ -0,0 +1,70 @@
+# cluster_KMeans.R
+# ::rtemis::
+# 2025 EDG rtemis.org
+
+#' K-means Clustering
+#'
+cluster_KMeans <- function(x, parameters) {
+  # Checks ----
+  check_is_S7(parameters, KMeansParameters)
+
+  # Dependencies ----
+  check_dependencies("flexclust")
+
+  # Data ----
+  check_unsupervised_data(x = x, allow_missing = TRUE)
+
+  # Cluster ----
+  clust <- flexclust::cclust(
+    x = x,
+    k = parameters$k,
+    dist = parameters$dist,
+    method = "kmeans"
+  )
+  check_inherits(clust, "kcca")
+  clust
+} # /rtemis::cluster_KMeans
+
+#' Hard Competitive Learning Clustering
+cluster_HardCL <- function(x, parameters) {
+  # Checks ----
+  check_is_S7(parameters, HardCLParameters)
+
+  # Dependencies ----
+  check_dependencies("flexclust")
+
+  # Data ----
+  check_unsupervised_data(x = x, allow_missing = TRUE)
+
+  # Cluster ----
+  clust <- flexclust::cclust(
+    x = x,
+    k = parameters$k,
+    dist = parameters$dist,
+    method = "hardcl"
+  )
+  check_inherits(clust, "kcca")
+  clust
+} # /rtemis::cluster_HardCL
+
+#' Neural Gas Clustering
+cluster_NeuralGas <- function(x, parameters) {
+  # Checks ----
+  check_is_S7(parameters, NeuralGasParameters)
+
+  # Dependencies ----
+  check_dependencies("flexclust")
+
+  # Data ----
+  check_unsupervised_data(x = x, allow_missing = TRUE)
+
+  # Cluster ----
+  clust <- flexclust::cclust(
+    x = x,
+    k = parameters$k,
+    dist = parameters$dist,
+    method = "neuralgas"
+  )
+  check_inherits(clust, "kcca")
+  clust
+} # /rtemis::cluster_NeuralGas
