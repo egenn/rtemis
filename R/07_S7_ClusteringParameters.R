cd0eef92633c9e576f03f164221aecc138a14293
Stathis Gennatas <gennatas@gmail.com>
2025-02-01 21:04:23 -0800

    init

---
+++ R/07_S7_ClusteringParameters.R
@@ -0,0 +1,175 @@
+# S7_ClusteringParameters.R
+# ::rtemis::
+# 2025 EDG rtemis.org
+
+# ClusteringParameters ----
+#' @title ClusteringParameters
+#'
+#' @description
+#' Clustering parameters class.
+#'
+#' @field algorithm Character: Algorithm name.
+#' @field parameters List: Algorithm-specific parameters.
+#'
+#' @author EDG
+ClusteringParameters <- new_class(
+  name = "ClusteringParameters",
+  properties = list(
+    algorithm = class_character,
+    parameters = class_list
+  )
+) # /ClusteringParameters
+
+# Make ClusteringParameters@parameters `$`-accessible
+method(`$`, ClusteringParameters) <- function(x, name) {
+  x@parameters[[name]]
+}
+
+# Make ClusteringParameters@parameters `[[`-accessible
+method(`[[`, ClusteringParameters) <- function(x, name) {
+  x@parameters[[name]]
+}
+
+# `$`-autocomplete ClusteringParameters@parameters ----
+.DollarNames.ClusteringParameters <- function(x, pattern = "") {
+  all_names <- names(x@parameters)
+  grep(pattern, all_names, value = TRUE)
+}
+method(`.DollarNames`, ClusteringParameters) <- function(x, pattern = "") {
+  .DollarNames.ClusteringParameters(x, pattern)
+}
+
+# Print ClusteringParameters ----
+#' Print ClusteringParameters
+#'
+#' Print method for ClusteringParameters object.
+#'
+#' @param x ClusteringParameters object.
+#' @param pad Integer: Left side padding.
+#'
+#' @author EDG
+#' @export
+method(print, ClusteringParameters) <- function(x, pad = 0L) {
+  objcat(paste(x@algorithm, "ClusteringParameters"), pad = pad)
+  printls(props(x)$parameters, pad = pad)
+  invisible(x)
+}
+
+# KMeansParameters ----
+#' @title KMeansParameters
+#'
+#' @description
+#' ClusteringParameters subclass for K-means Clustering.
+#'
+#' @author EDG
+#' @export
+KMeansParameters <- new_class(
+  name = "KMeansParameters",
+  parent = ClusteringParameters,
+  constructor = function(k, dist) {
+    k <- clean_posint(k)
+    check_inherits(dist, "character")
+    new_object(
+      ClusteringParameters(
+        algorithm = "KMeans",
+        parameters = list(
+          k = k,
+          dist = dist
+        )
+      )
+    )
+  }
+) # /KMeansParameters
+
+#' Setup KMmeansParameters
+#'
+#' @param k Number of clusters.
+#'
+#' @return KMeansParameters object.
+#'
+#' @author EDG
+#' @export
+setup_KMeans <- function(k = 3L, dist = c("euclidean", "manhattan")) {
+  k <- clean_posint(k)
+  dist <- match.arg(dist)
+  KMeansParameters(k, dist)
+} # /rtemis::setup_KMeans
+
+# HardCLParameters ----
+#' @title HardCLParameters
+#'
+#' @description
+#' ClusteringParameters subclass for HardCL Clustering.
+#'
+#' @author EDG
+#' @export
+HardCLParameters <- new_class(
+  name = "HardCLParameters",
+  parent = ClusteringParameters,
+  constructor = function(k, dist) {
+    k <- clean_posint(k)
+    check_inherits(dist, "character")
+    new_object(
+      ClusteringParameters(
+        algorithm = "HardCL",
+        parameters = list(
+          k = k,
+          dist = dist
+        )
+      )
+    )
+  }
+) # /HardCLParameters
+
+#' Setup HardCLParameters
+#'
+#' @param k Number of clusters.
+#'
+#' @return HardCLParameters object.
+#'
+#' @author EDG
+#' @export
+setup_HardCL <- function(k = 3L, dist = c("euclidean", "manhattan")) {
+  k <- clean_posint(k)
+  dist <- match.arg(dist)
+  HardCLParameters(k, dist)
+} # /rtemis::setup_HardCL
+
+# NeuralGasParameters ----
+#' @title NeuralGasParameters
+#'
+#' @description
+#' ClusteringParameters subclass for Neural Gas Clustering.
+#'
+#' @author EDG
+#' @export
+NeuralGasParameters <- new_class(
+  name = "NeuralGasParameters",
+  parent = ClusteringParameters,
+  constructor = function(k, dist) {
+    k <- clean_posint(k)
+    check_inherits(dist, "character")
+    new_object(
+      ClusteringParameters(
+        algorithm = "NeuralGas",
+        parameters = list(
+          k = k,
+          dist = dist
+        )
+      )
+    )
+  }
+) # /NeuralGasParameters
+
+#' Setup NeuralGasParameters
+#'
+#' @param k Number of clusters.
+#'
+#' @author EDG
+#' @return NeuralGasParameters object.
+#' @export
+setup_NeuralGas <- function(k = 3L, dist = c("euclidean", "manhattan")) {
+  k <- clean_posint(k)
+  dist <- match.arg(dist)
+  NeuralGasParameters(k, dist)
+} # /rtemis::setup_NeuralGas
