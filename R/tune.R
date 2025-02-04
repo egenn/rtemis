0b921f8787514fc2eb3a7b525b35abd2539b6e72
Stathis Gennatas <gennatas@gmail.com>
2025-02-04 10:41:00 -0800

    up

--- R/tune.R
+++ R/tune.R
@@ -3,9 +3,9 @@
 # 2025 EDG rtemis.org
 
 #' Get Tuner Function
-#' 
+#'
 #' @param type Character: Type of tuner.
-#' 
+#'
 #' @author EDG
 #' @keywords internal
 get_tuner_fn <- function(type = "GridSearch") {
@@ -16,32 +16,30 @@ get_tuner_fn <- function(type = "GridSearch") {
 } # /rtemis::get_tuner_fn
 
 #' Tune Supervised Learning Model
-#' 
-#' @param dat_training data.frame or similar: Training set data.
+#'
+#' @param x data.frame or similar: Training set data.
 #' @param hyperparameters `Hyperparameters` object: make using each learner's `setup_*` function.
 #' @param tuner_parameters `TunerParameters` object: created with [setup_GridSearch].
 #' @param verbosity Integer: Verbosity level.
-#' 
+#'
 #' @author EDG
 #' @export
 
 tune <- function(
-  dat_training,
-  hyperparameters,
-  tuner_parameters,
-  weights = NULL,
-  verbosity = 1
-) {
+    x,
+    hyperparameters,
+    tuner_parameters,
+    weights = NULL,
+    verbosity = 1L) {
   check_is_S7(hyperparameters, Hyperparameters)
   check_is_S7(tuner_parameters, TunerParameters)
   stopifnot(needs_tuning(hyperparameters))
   tuner_fn <- get_tuner_fn(tuner_parameters@type)
   do.call(tuner_fn, list(
-    dat_training = dat_training,
+    x = x,
     hyperparameters = hyperparameters,
     tuner_parameters = tuner_parameters,
     weights = weights,
     verbosity = verbosity
   ))
-
 } # /rtemis::tune
