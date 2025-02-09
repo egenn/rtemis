5dae4290032a6d06d20a8107d4fd13c26d315237
Stathis Gennatas <gennatas@gmail.com>
2025-02-09 12:36:34 -0800

    up

--- tests/testthat/test-Clustering.R
+++ tests/testthat/test-Clustering.R
@@ -39,6 +39,11 @@ test_that("cluster_KMeans() with k = 10 succeeds", {
   expect_s7_class(iris_kmeans10, Clustering)
 })
 
+# setup_HardCL ----
+test_that("setup_HardCL() succeeds", {
+  expect_s7_class(setup_HardCL(), HardCLParameters)
+})
+
 # cluster HardCL ----
 iris_hardcl <- cluster(x, algorithm = "HardCL")
 iris_hardcl
@@ -46,9 +51,27 @@ test_that("cluster_HardCL() succeeds", {
   expect_s7_class(iris_hardcl, Clustering)
 })
 
+# setup_NeuralGas ----
+test_that("setup_NeuralGas() succeeds", {
+  expect_s7_class(setup_NeuralGas(), NeuralGasParameters)
+})
+
 # cluster NeuralGas ----
 iris_neuralgas <- cluster(x, algorithm = "NeuralGas")
 iris_neuralgas
 test_that("cluster_NeuralGas() succeeds", {
   expect_s7_class(iris_neuralgas, Clustering)
+})
+
+# setup_CMeans ----
+test_that("setup_CMeans() succeeds", {
+  expect_s7_class(setup_CMeans(), CMeansParameters)
+})
+
+# cluster CMeans ----
+parameters <- setup_CMeans(k = 3L)
+parameters
+iris_cmeans <- cluster(x, algorithm = "CMeans")
+test_that("cluster_CMeans() succeeds", {
+  expect_s7_class(iris_cmeans, Clustering)
 })
