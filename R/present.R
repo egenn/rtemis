ba1a914d57a2d823480512d35ea8c2a2231807ce
Stathis Gennatas <gennatas@gmail.com>
2025-06-08 07:29:33 -0700

    docs

--- R/present.R
+++ R/present.R
@@ -2,15 +2,11 @@
 # ::rtemis::
 # 2025 EDG rtemis.org
 
-#' @name present
+#' Present list of Supervised or SupervisedRes objects
 #'
-#' @title
-#' Present multiple SupervisedRes objects
+#' Plot training and testing performance boxplots of multiple `Supervised` or `SupervisedRes` objects
 #'
-#' @description
-#' Plot training and testing performance boxplots of multiple `SupervisedRes` objects
-#'
-#' @param x List of SupervisedRes objects.
+#' @param x List of Supervised or SupervisedRes objects.
 #' @param metric Character: Metric to plot.
 #' @param model_names Character: Names of models being plotted.
 #' @param ylim Numeric vector of length 2: y-axis limits for the boxplots.
@@ -21,7 +17,7 @@
 #' @return plotly object
 #'
 #' @author EDG
-method(present, class_list) <- function(
+present.list <- function(
   x,
   metric = NULL,
   model_names = NULL,
@@ -117,4 +113,8 @@ method(present, class_list) <- function(
   }
 
   plt
-} # /rtemis::present
+} # /rtemis::present.list
+
+method(present, class_list) <- function(x, ...) {
+  present.list(x, ...)
+} # /rtemis::present.class_list
