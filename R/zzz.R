7ae95b656f35784304c6bc9816c3d24eb1fc850c
Stathis Gennatas <gennatas@gmail.com>
2025-07-27 19:43:07 -0700

    cleanup

--- R/zzz.R
+++ R/zzz.R
@@ -107,11 +107,6 @@ setup_progress <- function() {
       gray("Cite: "),
       # ' > citation("rtemis")',
       rtcitation,
-      # bold("\n  Setup"),
-      # "\n  \u2514 ",
-      # italic(gray("Enable progress reporting:")),
-      # " > progressr::handlers(global = TRUE)",
-      # '\n                               > progressr::handlers("cli")',
       "\n\n  ",
       red(bold("PSA:"), "Do not throw data at algorithms. Compute responsibly!")
     ))
